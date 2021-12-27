9c4b2c7d3e42631ba2912eb556224e446990bc9f
goromal <andrew.torgesen@gmail.com>
2021-12-26 16:59:01 -0800

    geometry package working

--- pkgs/python-packages/geometry/default.nix
+++ pkgs/python-packages/geometry/default.nix
@@ -7,9 +7,10 @@
 , python
 , buildPythonPackage
 }:
-callPackage ../pythonPkgFromPybind {
+callPackage ../pythonPkgFromPybind.nix {
     pname = "geometry";
     version = "1.0.0";
+    description = "Implementations for SO(3) and SE(3).";
     inherit stdenv;
     pkg-src = builtins.fetchGit (import ./src.nix);
     cppNativeBuildInputs = [
