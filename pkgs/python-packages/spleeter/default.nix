3ddd117e5c89d69e0f442d79f064868f776513ed
Andrew Torgesen <andrew.torgesen@gmail.com>
2021-12-26 22:31:39 -0800

    Dev/spleeter (#2)
    * needs spleeter default
    
    * spleeter builds, but runs with tensorflow error
    
    * spleeter working

--- pkgs/python-packages/spleeter/default.nix
+++ pkgs/python-packages/spleeter/default.nix
@@ -1,92 +1,27 @@
 # https://github.com/deezer/spleeter
 { buildPythonPackage
-, callPackage
 , fetchPypi
-, isPyPy
+, pythonAtLeast
 , pythonOlder
-, python
-, stdenv
-, lib
-, pytestCheckHook
-, pytest-xdist
-, shellingham
-, coverage
-, mypy
-, black
-, isort
-, locale
 , pandas
 , tensorflow
 , ffmpeg
 , libsndfile
-, llvm
-, enum34
-, isPy3k
 , ffmpeg-python
 , norbert
-, gfortran
-, pytest
-, blas
-, lapack
-, writeTextFile
-, cython
-, setuptoolsBuildHook
-, scipy
-, tensorflow-tensorboard
-, tensorflow-estimator
-, h5py
-, opt-einsum
-, keras
-, keras-preprocessing
-, pillow
+, typer
+, llvmlite
+, numpy
+, httpx
+, librosa
 }:
-let 
-    numpy_1_16_6 = callPackage ./overrides/numpy_1_17_5.nix { # TODO
-        inherit lib fetchPypi python buildPythonPackage gfortran pytest blas;
-        inherit lapack writeTextFile isPyPy cython setuptoolsBuildHook;
-    };
-    sp_patched = scipy.override { numpy = numpy_1_16_6; };
-    tftb_patched = tensorflow-tensorboard.override { numpy = numpy_1_16_6; };
-    tfes_patched = tensorflow-estimator.override { numpy = numpy_1_16_6; };
-    h5_patched = h5py.override { numpy = numpy_1_16_6; };
-    oe_patched = opt-einsum.override { numpy = numpy_1_16_6; };
-    pl_patched = pillow.override { numpy = numpy_1_16_6; };
-    kp_patched = keras-preprocessing.override {
-        numpy = numpy_1_16_6;
-        pillow = pl_patched;
-        scipy = sp_patched;
-        # keras ????
-    };
-    kr_patched = keras.override {
-        numpy = numpy_1_16_6;
-        scipy = sp_patched;
-        h5py = h5_patched;
-        keras-preprocessing = kp_patched;
-    };
-
-    tf_patched = tensorflow.override { 
-        numpy = numpy_1_16_6; 
-        tensorflow-tensorboard = tftb_patched;
-        tensorflow-estimator = tfes_patched;
-        h5py = h5_patched;
-        opt-einsum = oe_patched;
-        keras = kr_patched;
-        keras-preprocessing = kp_patched;
-    };
-    pd_patched = pandas.override { numpy = numpy_1_16_6; };
-    nb_patched = norbert.override { scipy = sp_patched; };
-    llvm-lite_0_36_0 = callPackage ./overrides/llvm-lite_0_36_0.nix {
-        inherit lib stdenv fetchPypi buildPythonPackage python llvm pythonOlder;
-        inherit isPyPy enum34 isPy3k;
-    };
-    click_7_1_2 = callPackage ./overrides/click_7_1_2.nix {
-        inherit lib buildPythonPackage fetchPypi locale pytestCheckHook;
-    };
-    typer_0_3_2 = callPackage ./overrides/typer_0_3_2.nix {
-        inherit lib stdenv buildPythonPackage fetchPypi black isort;
-        click = click_7_1_2;
-        inherit pytestCheckHook pytest-xdist shellingham coverage mypy;
-    };
+let
+    adjustDependency = ppackage: ppSpec: sSpec: pSpec: ''
+        substituteInPlace pyproject.toml --replace '${ppackage.pname}${ppSpec}' '${ppackage.pname} = "${ppackage.version}"'
+        substituteInPlace setup.py --replace '${ppackage.pname}${sSpec}' '${ppackage.pname}==${ppackage.version}'
+        substituteInPlace PKG-INFO --replace '${ppackage.pname}${pSpec}' '${ppackage.pname} (==${ppackage.version})'
+    '';
+    tfVers = tensorflow.version;
 in buildPythonPackage rec {
     pname = "spleeter";
     version = "2.3.0";
@@ -94,22 +29,25 @@ in buildPythonPackage rec {
         ffmpeg
         libsndfile
     ];
-    # Spleeter specifies >= 3.6, but flake8 tests fail on 3.7,
-    # and I don't want to deal with that currently.
-    disabled = pythonOlder "3.8";
+    disabled = pythonOlder "3.8" || pythonAtLeast "3.9";
     propagatedBuildInputs = [
-        numpy_1_16_6
-        pd_patched
-        typer_0_3_2
-        tf_patched
-        llvm-lite_0_36_0
+        numpy
+        pandas
+        typer
+        tensorflow
+        llvmlite
         ffmpeg-python
-        nb_patched
+        norbert
+        httpx
+        librosa
     ];
     postPatch = ''
-        substituteInPlace pyproject.toml --replace 'tensorflow = "2.5.0"' 'tensorflow = "2.7.0"'
-        substituteInPlace setup.py --replace 'tensorflow==2.5.0' 'tensorflow==2.7.0'
-        substituteInPlace PKG-INFO --replace 'tensorflow (==2.5.0)' 'tensorflow (==2.7.0)'
+        ${adjustDependency tensorflow " = \"2.5.0\"" "==2.5.0" " (==2.5.0)"}
+        ${adjustDependency typer " = \"^0.3.2\"" ">=0.3.2,<0.4.0" " (>=0.3.2,<0.4.0)"}
+        ${adjustDependency llvmlite " = \"^0.36.0\"" ">=0.36.0,<0.37.0" " (>=0.36.0,<0.37.0)"}
+        ${adjustDependency numpy " = \"<1.20.0,>=1.16.0\"" ">=1.16.0,<1.20.0" " (>=1.16.0,<1.20.0)"}
+        ${adjustDependency librosa " = \"0.8.0\"" "==0.8.0" " (==0.8.0)"}
+        substituteInPlace spleeter/model/functions/unet.py --replace 'tensorflow.compat.v1.keras' 'tensorflow.keras'
     '';
     src = fetchPypi {
         inherit pname version;
