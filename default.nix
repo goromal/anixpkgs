93c36910818ff4094b3c642fc30b4d2b40efde46
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-01-22 21:55:44 -0800

    make overlay a function (#9)

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
+{ overlays ? []
+, ...
+}@args:
 with import ./dependencies.nix;
 import nix-ros-overlay { 
   inherit nixpkgs;
-  overlays = [ (import ./overlay.nix) ];
-}
+  overlays = [ (import ./overlay.nix) ] ++ overlays;
+} // args
