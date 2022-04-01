0ae1e8ae320b102729efdd0d415801c0c9e11a25
Andrew Torgesen <atorgesen@anduril.com>
2022-04-01 09:09:05 -0700

    add colors to color-prints

--- pkgs/bash-packages/color-prints/default.nix
+++ pkgs/bash-packages/color-prints/default.nix
@@ -7,19 +7,27 @@ let
         writeShellScriptBin "echo_${colorname}" ''
             echo -e "\033[1;${builtins.toString colorcode}m$@\033[0m"
         '';
-    echo-green = echo-color "green" 32;
-    echo-cyan = echo-color "cyan" 36;
+    echo-black = echo-color "black" 30;
     echo-red = echo-color "red" 31;
+    echo-green = echo-color "green" 32;
     echo-yellow = echo-color "yellow" 33;
+    echo-blue = echo-color "blue" 34;
+    echo-magenta = echo-color "magenta" 35;
+    echo-cyan = echo-color "cyan" 36;
+    echo-white = echo-color "white" 37;
 in stdenv.mkDerivation {
     name = "color-prints";
     version = "1.0.0";
     unpackPhase = "true";
     installPhase = ''
         mkdir -p $out/bin
-        cp ${echo-green}/bin/echo_green $out/bin
-        cp ${echo-cyan}/bin/echo_cyan $out/bin
+        cp ${echo-black}/bin/echo_black $out/bin
         cp ${echo-red}/bin/echo_red $out/bin
+        cp ${echo-green}/bin/echo_green $out/bin
         cp ${echo-yellow}/bin/echo_yellow $out/bin
+        cp ${echo-blue}/bin/echo_blue $out/bin
+        cp ${echo-magenta}/bin/echo_magenta $out/bin
+        cp ${echo-cyan}/bin/echo_cyan $out/bin
+        cp ${echo-white}/bin/echo_white $out/bin
     '';
 }
