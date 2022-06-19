a4dc54dc792d31ea14037f9d6b9de26896ce311b
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-06-19 13:58:47 -0700

    state self-operators

--- pkgs/cxx-packages/signals-cpp/src.nix
+++ pkgs/cxx-packages/signals-cpp/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/signals-cpp.git";
-    rev = "e6e4d948a478a26100686c76455ba4577860f3d2";
+    rev = "b9f698ce6182f4d3ad13d92d6ccdc6fe0990eeb9";
     ref = "master";
 }
