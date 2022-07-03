4f0d969d9ddee9a75ae3f154dc3177169d00164b
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-03 11:55:32 -0700

    fix tangent vector dimension for test

--- pkgs/python-packages/geometry/src.nix
+++ pkgs/python-packages/geometry/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/geometry.git";
-    rev = "ef8211504cd18ebf332154b00090cb8507b57ddf";
+    rev = "15362abe60d42052b5bbe9372e2fef5afbdc5edb";
     ref = "dev/so2";
 }
