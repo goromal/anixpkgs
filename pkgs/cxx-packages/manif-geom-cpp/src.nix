e7b4f41df6454fe2cd71c73cdb5c1537e59d92dd
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-02 21:51:37 -0700

    Function name

--- pkgs/cxx-packages/manif-geom-cpp/src.nix
+++ pkgs/cxx-packages/manif-geom-cpp/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/manif-geom-cpp.git";
-    rev = "fbdef21081bd427dbc7b891fde118f381b75a96b";
+    rev = "1af07b3086e3ccadf1014776e748eb0e26a78535";
     ref = "dev/so2";
 }
