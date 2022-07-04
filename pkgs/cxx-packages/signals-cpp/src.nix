091d151191f6a348e3b676e37c50820dfa93f0fe
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-04 13:49:09 -0700

    Fix underscore

--- pkgs/cxx-packages/signals-cpp/src.nix
+++ pkgs/cxx-packages/signals-cpp/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/signals-cpp.git";
-    rev = "dad4b75466836a766a393a692fb5c7190a7ec9b3";
+    rev = "b2cd7a6a7c1643b7e37a65ea2c50c7872bdea198";
     ref = "dev/so2";
 }
