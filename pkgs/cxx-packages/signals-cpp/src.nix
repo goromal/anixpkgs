888f20fc1bcddc3261318b1996969e6d668e04bb
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-03 20:40:50 -0700

    Add SO(2) to signals-cpp

--- pkgs/cxx-packages/signals-cpp/src.nix
+++ pkgs/cxx-packages/signals-cpp/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/signals-cpp.git";
-    rev = "b9f698ce6182f4d3ad13d92d6ccdc6fe0990eeb9";
-    ref = "master";
+    rev = "c0fe1b78d18bcef5f6080158fde391a947304038";
+    ref = "dev/so2";
 }
