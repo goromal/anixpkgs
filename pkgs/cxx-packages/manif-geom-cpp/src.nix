61115239be2b90dce86e10e28e52b99073a821f1
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-09 11:46:27 -0400

    SE(2) Implementations

--- pkgs/cxx-packages/manif-geom-cpp/src.nix
+++ pkgs/cxx-packages/manif-geom-cpp/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/manif-geom-cpp.git";
-    rev = "151e3df09f2ba024ac959ca6c4b06a735af82bda";
-    ref = "master";
+    rev = "58c5d554f660eb2879135797a1002cb4ff0bb76b";
+    ref = "dev/se2";
 }
