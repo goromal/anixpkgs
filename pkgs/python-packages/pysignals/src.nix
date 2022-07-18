74cd30e96c69e04aba2502dc3a1fa7903039f75c
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-07-17 21:20:52 -0700

    update pysignals

--- pkgs/python-packages/pysignals/src.nix
+++ pkgs/python-packages/pysignals/src.nix
@@ -1,5 +1,5 @@
 {
     url = "git@github.com:goromal/pysignals.git";
-    rev = "71ff61bdf3df04f434e1ef1ece3749a7401d33dd";
-    ref = "master";
+    rev = "ec6b0c42569997aa84d22bd6181389af9cd0a44d";
+    ref = "dev/se2";
 }
