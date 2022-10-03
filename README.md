229fef6613a30a9cee0602bed607fa67f30a5c85
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-10-02 21:27:13 -0700

    Machines refactor for personal laptop (#33)
    * Machines refactor for personal laptop tests
    
    * Update README.md
    
    * Inspiron setup complete

--- README.md
+++ README.md
@@ -33,3 +33,87 @@ let
 in
 python-with-my-packages.env
 ```
+
+## Installation Instructions on a New Machine
+
+*Sources*
+
+- https://nixos.wiki/wiki/NixOS_Installation_Guide
+- https://alexherbo2.github.io/wiki/nixos/install-guide/
+
+1. Download a [NixOS ISO](https://nixos.org/nixos/download.html) image.
+2. Plug in a USB stick large enough to accommodate the image.
+3. Find the right device with `lsblk` or `fdisk -l`. Replace `/dev/sdX` with the proper device (do not use `/dev/sdX1` or partitions of the disk; use the whole disk `/dev/sdX`).
+4. Burn ISO to USB stick with 
+```bash
+cp nixos-xxx.iso /dev/sdX
+# OR
+dd if=nixos.iso of=/dev/sdX bs=4M status=progress conv=fdatasync
+```
+5. On the new machine, one-time boot UEFI into the USB stick on the computer (will need to disable Secure Boot from BIOS first)
+6. Wipe the file system: 
+```bash
+wipefs [--all -a] /dev/sda
+```
+7. `gparted`
+   1. Create a GUID table: *Device* > *Create Partition Table* > *GPT*
+      1. Select `/dev/sda`
+      2. *Entire disk*
+   2. Create the boot partition: *Partition* > *New*
+      1. Free space preceding (MiB): 1
+      2. New size (MiB): 512
+      3. Free spae following (MiB): Rest
+      4. Align to: MiB
+      5. Create as: Primary Partition
+      6. Partition name: EFI
+      7. File system: `fat32`
+      8. Label: EFI
+   3. Add the `boot` flag
+      1. Right-click on `/dev/sda1` to manage flags
+      2. Add the `boot` flag and enable `esp` (should be automatic with GPT)
+   4. Create the root partition: *Partition* > *New*
+      1. Free space preceding (MiB): 0
+      2. New size (MiB): Rest
+      3. Free space following (MiB): 0
+      4. Align to: MiB
+      5. Create as: Primary Partition
+      6. Partition name: NixOS
+      7. File system: `ext4`
+      8. Label: NixOS
+   5. Apply modifications
+8. Mount root and boot partitions:
+```bash
+mkdir /mnt/nixos
+mount /dev/disk/by-label/NixOS /mnt/nixos
+mkdir /mnt/nixos/boot
+mount /dev/disk/by-label/EFI /mnt/nixos/boot
+```
+9. Generate an initial configuration (you'll want it to enable WiFi connectivity and a web browser at least):
+```bash
+nixos-generate-config --root /mnt/nixos
+# /etc/nixos/configuration.nix
+# /etc/nixos/hardware-configuration.nix
+```
+10. Do the installation:
+```bash
+nixos-install --root /mnt/nixos
+```
+11. If everything went well:
+```bash
+reboot
+```
+12. Log into Github and generate an SSH key for authentication.
+13. Clone and link an editable version of the configuration:
+```bash
+mkdir -p /data/andrew/sources # or in an alternate location, for now
+git clone git@github.com:goromal/anixpkgs.git /data/andrew/sources/anixpkgs
+cat /etc/nixos/hardware-configuration.nix > /data/andrew/sources/anixpkgs/pkgs/nixos/personal/[hardware-configuration.nix] # update link/headings in configuration.nix
+sudo mv /etc/nixos/configuration.nix /etc/nixos/old.configuration.nix
+sudo mv /etc/nixos/hardware-configuration.nix /etc/nixos/old.hardware-configuration.nix
+sudo ln -s /data/andrew/sources/anixpkgs/pkgs/nixos/personal/configuration.nix /etc/nixos/configuration.nix
+```
+14. Make other needed updates to the configuration, then apply:
+```bash
+sudo nixos-rebuild boot
+sudo reboot
+```
--- pkgs/nixos/personal/configuration.nix
+++ pkgs/nixos/personal/configuration.nix
@@ -2,153 +2,253 @@
 with pkgs;
 with lib;
 let
-    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+    ##################################################################
+    orig-nixos-version = "22.05"; # Should match system.stateVersion 
+                                  # of original config
+    nixos-version = "22.05"; # Should match the channel in <nixpkgs>
+    anix-version  = "0.0.0"; # Whatever you want
+    hardware-config = ./hardware-configuration-inspiron.nix;
+    ##################################################################
+    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-${nixos-version}.tar.gz";
+    anixpkgs     = import (builtins.fetchTarball "https://github.com/goromal/anixpkgs/archive/refs/tags/v${anix-version}.tar.gz") {
+        config.allowUnfree = true;
+    };
+    play-zelda = writeShellScriptBin "playzelda" ''
+        ${dolphinEmu}/bin/dolphin-emu -e /data/andrew/Dropbox/Games/LegendOfZeldaCollectorsEdition.iso
+    '';
 in
 {
     imports = [
+        hardware-config
         ../base.nix
         (import "${home-manager}/nixos")
     ];
 
-    config = {
-        networking.hostName = "personal";
+    nix.nixPath = lib.mkForce [
+        "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+        "nixos-config=/data/andrew/sources/anixpkgs/pkgs/nixos/personal/configuration.nix"
+        "anixpkgs=/data/andrew/sources/anixpkgs"
+    ];
 
-        services.xserver.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
-        services.xserver.displayManager.gdm.enable = true;
-        programs.ssh.setXAuthLocation = true;
+    networking.hostName = "atorgesen-laptop";
+    boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    networking.networkmanager.enable = true;
 
-        services.dbus.packages = [ dconf ];
-        programs.dconf.enable = true;
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-        environment.gnome.excludePackages = [
-            gnome.cheese
-            gnome.gnome-music
-            epiphany
-            gnome.totem 
-            gnome.tali 
-            gnome.iagno 
-            gnome.hitori 
-            gnome.atomix 
-            gnome-tour  
-            gnome.geary 
-        ];
+    # Configure keymap in X11
+    # services.xserver.layout = "us";
+    # services.xserver.xkbOptions = "eurosign:e";
 
-        home-manager.users.andrew = {
-            programs.home-manager.enable = true;
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
 
-            gtk = { # TODO doesn't show up
-                enable = true;
-                theme = {
-                    name = "Nordic";
-                    package = nordic;
-                };
+    # Enable CUPS to print documents.
+    # services.printing.enable = true;
+
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+
+    services.udev.packages = [ pkgs.dolphinEmu ];
+
+    home-manager.users.andrew = {
+        programs.home-manager.enable = true;
+
+        # nixpkgs/pkgs/data/themes
+        # nixpkgs/pkgs/data/icons
+        gtk = {
+            enable = true;
+            iconTheme = {
+                name = "Numix-Circle";
+                package = pkgs.numix-icon-theme-circle;
             };
-
-            home.packages = [
-                vlc
-                evince
-                inkscape
-                dropbox # TODO configure
-                vscodium
-                chromium
-                google-chrome
-                gnumake
-                gcc
-                gdb
-                cmake
-                valgrind
-                graphviz
-                imagemagick
-                xclip
-                gimp
-                simplescreenrecorder
-                pinta
-                pandoc
-                # texlive.combined.scheme-full TODO enable, add vscodium plugin
-            ];
-
-            # TODO vscodium default settings
-
-            # TODO add text file creation to context menu
-
-            programs.git = {
-                package = gitAndTools.gitFull;
-                enable = true;
-                userName = "Andrew Torgesen";
-                userEmail = "andrew.torgesen@gmail.com";
+            theme = {
+                name = "Nordic";
+                package = pkgs.nordic;
             };
-
-            programs.command-not-found.enable = true;
-
-            programs.vim = {
-                enable = true;
-                extraConfig = ''
-                    if has('gui_running')
-                        set guifont=Iosevka
-                    endif
-
-                    set expandtab
-
-                    " open NERDTree automatically if no file specified
-                    "autocmd StdinReadPre * let s:std_in=1
-                    "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
-
-                    " open NERDTree on Ctrl-n
-                    map <C-n> :NERDTreeToggle<CR>
-
-                    set wildignore+=*/node_modules/*,_site,*/__pycache__/,*/venv/*,*/target/*,*/.vim$,\~$,*/.log,*/.aux,*/.cls,*/.aux,*/.bbl,*/.blg,*/.fls,*/.fdb*/,*/.toc,*/.out,*/.glo,*/.log,*/.ist,*/.fdb_latexmk
-                    set encoding=utf-8
-                    set termguicolors
-                    set background=dark
-                    let g:mix_format_on_save = 1
-                    let g:mix_format_options = '--check-equivalent'
-                '';
-                settings = {
-                    relativenumber = true;
-                    number = true;
-                };
-                plugins = with vimPlugins; [
-                    vim-elixir
-                    # vim-mix-format
-                    sensible
-                    vim-airline
-                    The_NERD_tree
-                    fugitive
-                    vim-gitgutter
-                    YouCompleteMe
-                    vim-abolish
-                    command-t
+        };
+        dconf.settings = {
+            "org/gnome/desktop/background" = {
+                "picture-uri" = "/data/andrew/.background-image";
+            };
+            "org/gnome/desktop/screensaver" = {
+                "picture-uri" = "/data/andrew/.background-image";
+            };
+            "org/gnome/desktop/wm/preferences" = {
+                "button-layout" = ":minimize,maximize,close";
+            };
+            "org/gnome/desktop/interface" = {
+                "clock-format" = "12h";
+            };
+            "org/gnome/shell" = {
+                "favorite-apps" = [
+                    "org.gnome.Nautilus.desktop"
+                    "google-chrome.desktop"
+                    "terminator.desktop"
+                    "codium.desktop"
+                    "pinta.desktop"
+                    "gimp.desktop"
+                    "org.inkscape.Inkscape.desktop"
                 ];
             };
+        };
 
-            systemd.user.services.dropbox = {
-                Unit = {
-                    Description = "Dropbox";
-                    After = [ "graphical-session-pre.target" ];
-                    PartOf = [ "graphical-session.target" ];
-                };
-                Service = {
-                    Restart = "on-failure";
-                    RestartSec = 1;
-                    ExecStart = "${dropbox}/bin/dropbox";
-                    Environment = "QT_PLUGIN_PATH=/run/current-system/sw/${qt5.qtbase.qtPluginPrefix}";
-                };
-                Install = {
-                    WantedBy = [ "graphical-session.target" ];
-                };
-            };
+        home.packages = [
+            ## upstream
+            gnome3.gnome-tweaks
+            vlc
+            evince
+            inkscape
+            chromium
+            maestral
+            google-chrome
+            direnv
+            gnumake
+            graphviz
+            imagemagick
+            terminator
+            xclip
+            gimp
+            simplescreenrecorder
+            pinta
+            pandoc
+            texlive.combined.scheme-full
+            docker
+            ## my packages
+            anixpkgs.color-prints
+            anixpkgs.git-cc
+            anixpkgs.md2pdf
+            anixpkgs.notabilify
+            anixpkgs.make-title
+            anixpkgs.pb
+            anixpkgs.code2pdf
+            anixpkgs.abc
+            anixpkgs.doku
+            anixpkgs.epub
+            anixpkgs.gif
+            anixpkgs.html
+            anixpkgs.md
+            anixpkgs.mp3
+            anixpkgs.mp4
+            anixpkgs.pdf
+            anixpkgs.png
+            anixpkgs.svg
+            anixpkgs.zipper
+            anixpkgs.fix-perms
+            anixpkgs.secure-delete
+            anixpkgs.sunnyside
+            anixpkgs.scrape
+            anixpkgs.manage-gmail
+            play-zelda
+        ];
 
-            home.file = {
-                ".tmux.conf" = {
-                    text = ''
-                        set-option -g default-shell /run/current-system/sw/bin/fish
-                        set-window-option -g mode-keys vi
-                        set -g default-terminal "screen-256color"
-                        set -ga terminal-overrides ',screen-256color:Tc'
-                    '';
+        # https://search.nixos.org/packages?channel=22.05&from=0&size=50&sort=relevance&type=packages&query=vscode-extensions
+        programs.vscode = {
+            enable = true;
+            package = vscodium;
+            extensions = with vscode-extensions; [
+                jnoortheen.nix-ide
+                yzhang.markdown-all-in-one
+                xaver.clang-format
+                ms-python.python
+                valentjn.vscode-ltex
+                llvm-vs-code-extensions.vscode-clangd
+                b4dm4n.vscode-nixpkgs-fmt
+            ];
+        };
+
+        programs.git = {
+            package = gitAndTools.gitFull;
+            enable = true;
+            userName = "Andrew Torgesen";
+            userEmail = "andrew.torgesen@gmail.com";
+            extraConfig = {
+                init = {
+                    defaultBranch = "master";
                 };
             };
         };
+
+        programs.command-not-found.enable = true;
+
+        programs.vim = {
+            enable = true;
+            extraConfig = ''
+                if has('gui_running')
+                    set guifont=Iosevka
+                endif
+                set expandtab
+                " open NERDTree automatically if no file specified
+                "autocmd StdinReadPre * let s:std_in=1
+                "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+                " open NERDTree on Ctrl-n
+                map <C-n> :NERDTreeToggle<CR>
+                set wildignore+=*/node_modules/*,_site,*/__pycache__/,*/venv/*,*/target/*,*/.vim$,\~$,*/.log,*/.aux,*/.cls,*/.aux,*/.bbl,*/.blg,*/.fls,*/.fdb*/,*/.toc,*/.out,*/.glo,*/.log,*/.ist,*/.fdb_latexmk
+                set encoding=utf-8
+                set termguicolors
+                set background=dark
+                let g:mix_format_on_save = 1
+                let g:mix_format_options = '--check-equivalent'
+            '';
+            settings = {
+                number = true;
+            };
+            plugins = with vimPlugins; [
+                vim-elixir
+                sensible
+                vim-airline
+                The_NERD_tree
+                fugitive
+                vim-gitgutter
+                YouCompleteMe
+                vim-abolish
+                command-t
+            ];
+        };
+
+        home.file = {
+            ".tmux.conf" = {
+                text = ''
+                    set-option -g default-shell /run/current-system/sw/bin/fish
+                    set-window-option -g mode-keys vi
+                    set -g default-terminal "screen-256color"
+                    set -ga terminal-overrides ',screen-256color:Tc'
+                '';
+            };
+            ".background-image" = {
+                source = ../res/wallpaper.jpg;
+            };
+            "Templates/EmptyDocument" = {
+                text = "";
+            };
+            ".config/VSCodium/User/settings.json" = {
+                text = ''
+                {
+                    "editor.formatOnSave": true,
+                    "editor.minimap.enabled": false,
+                    "clang-format.language.cpp.enable": true,
+                    "clang-format.executable": "/run/current-system/sw/bin/clang-format",
+                    "clang-format.language.cpp.style": "",
+                    "window.zoomLevel": -1
+                }
+                '';
+            };
+        };
     };
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = orig-nixos-version; # Did you read the comment?    
 }
