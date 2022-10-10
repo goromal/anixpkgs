34059acce79c48652689ba7de0b704573b1dfbc8
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-10-09 21:31:35 -0700

    add pybind template to cpp-helper

--- pkgs/bash-packages/cpp-helper/default.nix
+++ pkgs/bash-packages/cpp-helper/default.nix
@@ -11,12 +11,14 @@ let
         usage: ${pkgname} [options]
 
         Options:
-        --make-format-file       Dumps a format rules file into ./.clang-format
-        --make-header-lib NAME   Generate a header-only library template
+        --make-format-file             Dumps a format rules file into .clang-format
+        --make-header-lib CPPNAME      Generate a header-only library template
+        --make-pybind-lib NAME,CPPNAME Generate a pybind package wrapping a header-only library
         '';
         optsWithVarsAndDefaults = [
             { var = "makeff"; isBool = true; default = "0"; flags = "--make-format-file"; }
             { var = "makehot"; isBool = false; default = ""; flags = "--make-header-lib"; }
+            { var = "makepbl"; isBool = false; default = ""; flags = "--make-pybind-lib"; }
         ];
     };
     printErr = "${color-prints}/bin/echo_red";
@@ -49,10 +51,48 @@ let
         mv "$makehot/cmake/example-cppConfig.cmake.in" "$makehot/cmake/''${makehot}Config.cmake.in"
     fi
     '';
+    makepblRule = ''
+    if [[ ! -z "$makepbl" ]]; then
+        if [[ "$makepbl" != *","* ]]; then
+            ${printErr} "Pybind names not delimited by a comma"
+            exit 1
+        fi
+        IFS=',' read -ra pblargs <<< "$makepbl"
+        pblname="''${pblargs[0]}"
+        cppname="''${pblargs[1]}"
+        if [[ -z "$pblname" ]]; then
+            ${printErr} "Pybind library name not specified"
+            exit 1
+        fi
+        if [[ -z "$cppname" ]]; then
+            ${printErr} "Wrapped c++ library name not specified"
+            exit 1
+        fi
+        if [[ -d "$pblname" ]]; then
+            while true; do
+                read -p "Destination directory exists ($pblname); remove? [yn] " yn
+                case $yn in
+                    [Yy]* ) rm -rf "$pblname"; break;;
+                    [Nn]* ) echo "Aborting."; exit;;
+                    * ) ${printErr} "Please respond y or n";;
+                esac
+            done
+        fi
+        ${printGrn} "Generating pybind wrapper library boilerplate for $pblname wrapping $cppname..."
+        git clone git@github.com:goromal/example_cpp_py.git "$tmpdir/example_cpp_py" ${redirects.suppress_all}
+        ${git-cc}/bin/git-cc "$tmpdir/example_cpp_py" "$pblname" ${redirects.suppress_all}
+        sed -i 's|example_cpp_py|'"$pblname"'|g' "$pblname/python_module.cpp"
+        sed -i 's|example-cpp|'"$cppname"'|g' "$pblname/python_module.cpp"
+        sed -i 's|example_cpp_py|'"$pblname"'|g' "$pblname/README.md"
+        sed -i 's|example_cpp_py|'"$pblname"'|g' "$pblname/CMakeLists.txt"
+        sed -i 's|example-cpp|'"$cppname"'|g' "$pblname/CMakeLists.txt"
+    fi
+    '';
 in writeShellScriptBin pkgname ''
     ${argparse}
     tmpdir=$(mktemp -d)
     ${makeffRule}
     ${makehotRule}
+    ${makepblRule}
     rm -rf "$tmpdir"
 ''
