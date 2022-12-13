d89230aebb1718c8d4f6b8b2259d747f22885086
Andrew Torgesen <andrew.torgesen@gmail.com>
2022-12-12 21:12:30 -0800

    fix rpi4 base import

--- pkgs/nixos/raspberryPi/4/base.nix
+++ pkgs/nixos/raspberryPi/4/base.nix
@@ -5,7 +5,7 @@ with lib;
 {
     imports = [
         "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"
-        ./piBase.nix
+        ../base.nix
     ];
 
     # Enable GPU acceleration
