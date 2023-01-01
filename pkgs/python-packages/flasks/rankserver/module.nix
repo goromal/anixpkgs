3e7f77b9ec555463b4fb4a6a581bf60ff6d79d8c
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-01-01 12:56:58 -0800

    define module

--- pkgs/python-packages/flasks/rankserver/module.nix
+++ pkgs/python-packages/flasks/rankserver/module.nix
@@ -1,3 +1,64 @@
-# TODO
-# option to open the firewall port
-# no root dir, just set the data dir directly
+{ pkgs, config, lib, ... }:
+with pkgs;
+with lib;
+let
+    cfg = config.services.rankserver;
+in
+{
+    options.services.rankserver = with types; {
+        enable = mkEnableOption "enable rank server";
+        package = mkOption {
+            type = types.package;
+            description = "The rank server package to use";
+            default = pkgs.rankserver;
+        };
+        homeDir = mkOption {
+            type = types.str;
+            description = "Home directory (will be cwd of the server)";
+            default = "/data/andrew";
+        };
+        dataDir = mkOption {
+            type = types.str;
+            description = "Data directory with rankable elements (.png|.txt). RELATIVE path from ~.";
+            default = "rankables";
+        };
+        port = mkOption {
+            type = types.int;
+            description = "Port for the server to use";
+            default = 5000;
+        };
+        openFirewall = mkOption {
+            type = types.bool;
+            description = "Whether to open the specific firewall port for inter-computer usage";
+            default = false;
+        };
+    };
+
+    config = mkIf cfg.enable {
+        networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [
+            cfg.port
+        ];
+        systemd.services.rankserver = {
+            enable = true;
+            description = "Rank server";
+            unitConfig = 
+            {
+                StartLimitIntervalSec = 0;
+            };
+            serviceConfig =
+            {
+                Type = "simple";
+                ExecStart = "${cfg.package}/bin/rankserver --port ${builtins.toString cfg.port} --data-dir ${cfg.dataDir}";
+                ReadWritePaths = [
+                    "${cfg.dataDir}"
+                ];
+                WorkingDirectory = cfg.homeDir;
+                Restart = "always";
+                RestartSec = 5;
+                User = "andrew";
+                Group = "dev";
+            };
+            wantedBy = [ "multi-user.target" ];
+        };
+    };
+}
