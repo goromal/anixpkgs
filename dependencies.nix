a1d27bd0c581adf8be125c62ab8952fb011e66df
Andrew Torgesen <atorgesen@anduril.com>
2023-01-21 17:27:45 -0800

    nixpkgs version from 12.14.22

--- dependencies.nix
+++ dependencies.nix
@@ -1,7 +1,7 @@
 {
     nixpkgs = builtins.fetchGit {
         url = "git@github.com:NixOS/nixpkgs.git";
-        rev = "faf0031868410414c5c11bae8a374de8af2ed68b";
+        rev = "e2f7be9877cb0851a46279d563abc90c0396253a";
         ref = "master";
     };
     nix-ros-overlay = builtins.fetchGit {
