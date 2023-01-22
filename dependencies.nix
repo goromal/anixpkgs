1e92ec23a3c192d294f7fa5391d22562f787845b
Andrew Torgesen <atorgesen@anduril.com>
2023-01-21 17:28:11 -0800

    add comment

--- dependencies.nix
+++ dependencies.nix
@@ -1,7 +1,7 @@
 {
     nixpkgs = builtins.fetchGit {
         url = "git@github.com:NixOS/nixpkgs.git";
-        rev = "e2f7be9877cb0851a46279d563abc90c0396253a";
+        rev = "e2f7be9877cb0851a46279d563abc90c0396253a"; # 12-14-22
         ref = "master";
     };
     nix-ros-overlay = builtins.fetchGit {
