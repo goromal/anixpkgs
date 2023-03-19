5f1fbf8826be19286105fd2aa73818ce8ccebf55
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-03-18 23:30:36 -0700

    switch typos

--- pkgs/nixos/pi/4/base.nix
+++ pkgs/nixos/pi/4/base.nix
@@ -19,7 +19,6 @@ in {
             nmcli d wifi connect LANtasia password 2292238177 ifname wlp1s0u1u1
         '')
         (writeShellScriptBin "switch-rpi4-configuration" ''
-            set -e pipefail
             configPath="$1"
             if [[ -z "$configPath" ]]; then
                 echo_red "Path to configuration.nix file not provided."
@@ -30,8 +29,8 @@ in {
             sysConfigPath=$(nix-build '<nixpkgs/nixos>' -A config.system.build.toplevel -I nixos-config=$configPath --no-out-link)
             echo "System configuration path: $sysConfigPath"
             echo_yellow "Switching configuration..."
-            sudo nix-env -p /nix/var/nix/profiles/system --set "/nix/store/''${sysConfigPath}"
-            sudo "/nix/store/''${sysConfigPath}/bin/switch-to-configuration" boot
+            sudo nix-env -p /nix/var/nix/profiles/system --set "''${sysConfigPath}"
+            sudo "''${sysConfigPath}/bin/switch-to-configuration" boot
             echo_yellow "...Done! Reboot for the changes to take effect."
         '')
     ];
