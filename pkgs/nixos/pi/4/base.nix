c6401e8ead3f00d0f0d722536ff8f48e760e0e47
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-03-18 22:58:34 -0700

    disable machines CI test; add pi4 switcher stub

--- pkgs/nixos/pi/4/base.nix
+++ pkgs/nixos/pi/4/base.nix
@@ -18,5 +18,17 @@ in {
         (writeShellScriptBin "wifi-connect" ''
             nmcli d wifi connect LANtasia password 2292238177 ifname wlp1s0u1u1
         '')
+        (writeShellScriptBin "switch-rpi4-configuration" ''
+            configPath="$1"
+            if [[ -z "$configPath" ]]; then
+                echo_red "Path to configuration.nix file not provided."
+                exit 1
+            fi
+            echo_yellow "Building and switching to configuration $configPath..."
+            export NIXPKGS_ALLOW_UNFREE=1
+            sysConfigPath=$(nix-build '<nixpkgs/nixos>' -A config.system.build.toplevel -I nixos-config=$configPath --no-out-link)
+            echo "System configuration path: $sysConfigPath"
+            echo_red "TODO the rest..."
+        '')
     ];
 }
