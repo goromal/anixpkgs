5339360308b9abeec756688608b2fa5c0e129e27
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-03-18 23:25:20 -0700

    finish rpi4 switcher

--- pkgs/nixos/pi/4/base.nix
+++ pkgs/nixos/pi/4/base.nix
@@ -19,6 +19,7 @@ in {
             nmcli d wifi connect LANtasia password 2292238177 ifname wlp1s0u1u1
         '')
         (writeShellScriptBin "switch-rpi4-configuration" ''
+            set -e pipefail
             configPath="$1"
             if [[ -z "$configPath" ]]; then
                 echo_red "Path to configuration.nix file not provided."
@@ -28,7 +29,10 @@ in {
             export NIXPKGS_ALLOW_UNFREE=1
             sysConfigPath=$(nix-build '<nixpkgs/nixos>' -A config.system.build.toplevel -I nixos-config=$configPath --no-out-link)
             echo "System configuration path: $sysConfigPath"
-            echo_red "TODO the rest..."
+            echo_yellow "Switching configuration..."
+            sudo nix-env -p /nix/var/nix/profiles/system --set "/nix/store/''${sysConfigPath}"
+            sudo "/nix/store/''${sysConfigPath}/bin/switch-to-configuration" boot
+            echo_yellow "...Done! Reboot for the changes to take effect."
         '')
     ];
 }
