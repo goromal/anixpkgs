2811093ebcbdfa22787b86d40f590b3ecc378016
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-03-20 22:42:42 -0700

    add useful base.nix git options (#60)

--- pkgs/nixos/base.nix
+++ pkgs/nixos/base.nix
@@ -205,9 +205,22 @@ in
             enable = true;
             userName = "Andrew Torgesen";
             userEmail = "andrew.torgesen@gmail.com";
+            aliases = {
+                aa = "add -A";
+                cm = "commit -m";
+                co = "checkout";
+                s = "status";
+                d = "diff";
+            };
             extraConfig = {
                 init = {
                     defaultBranch = "master";
+                };
+                push = {
+                    default = "current";
+                };
+                pull = {
+                    default = "current";
                 };
             };
         };
