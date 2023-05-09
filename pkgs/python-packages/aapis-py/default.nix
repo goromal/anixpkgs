43cecd5b42a8cb569bbe197219dde6ddf7a53d5a
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-05-09 09:38:38 -0700

    grpc support in python bindings

--- pkgs/python-packages/aapis-py/default.nix
+++ pkgs/python-packages/aapis-py/default.nix
@@ -1,9 +1,31 @@
 { buildPythonPackage
+, buildPython
 , protobuf
 , apis-fds
 , pkg-src
+, grpc-support ? true
 }:
-buildPythonPackage rec {
+let
+    bldCmd = if grpc-support then (
+        let 
+            py = buildPython.withPackages(
+                p: with p; [ mypy-protobuf grpcio-tools ]
+            );
+        in ''
+        ${py}/bin/python3 -m grpc_tools.protoc                    \
+            --proto_path=protos                                   \
+            --plugin=${py.pkgs.mypy-protobuf}/bin/protoc-gen-mypy \
+            --python_out=aapis_py                                 \
+            --grpc_python_out=aapis_py                            \
+            --mypy_out=aapis_py                                   \
+            protos/**/*.proto
+    ''
+    ) else ''
+        for proto in **/*.proto; do
+            protoc -I protos --python_out=aapis_py $proto
+        done
+    '';
+in buildPythonPackage rec {
     pname = "aapis-py";
     version = "0.0.0";
     src = pkg-src;
@@ -17,9 +39,7 @@ buildPythonPackage rec {
         touch aapis_py/aapis_fds/__init__.py
         touch aapis_py/aapis_fds/py.typed
         cp ${apis-fds} aapis_py/aapis_fds/aapis.desc
-        for proto in **/*.proto; do
-            protoc -I protos --python_out=aapis_py $proto
-        done
         touch aapis_py/aapis/py.typed
+        ${bldCmd}
     '';
 }
