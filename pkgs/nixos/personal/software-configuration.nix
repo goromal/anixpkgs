f9cb2b045d4901db13ec5108f732cbf24e8ce697
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-05-19 21:58:44 -0700

    support apple airpods on personal computer (#77)

--- pkgs/nixos/personal/software-configuration.nix
+++ pkgs/nixos/personal/software-configuration.nix
@@ -48,9 +48,21 @@ with import ../dependencies.nix { inherit config; };
 
     services.lorri.enable = true;
 
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
-    nixpkgs.config.pulseaudio = true;
+    # Specialized bluetooth and sound settings for Apple AirPods
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.settings = {
+        General = {
+            ControllerMode = "bredr";
+        };
+    };
+    services.blueman.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        pulse.enable = true;
+    };
 
     services.udev.packages = [ pkgs.dolphinEmu ];
 
