5d0c5d4d50e766aae89a9650606b4ecf5191c059
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-06-01 22:21:38 -0700

    Update dependencies.nix

--- dependencies.nix
+++ dependencies.nix
@@ -1,6 +1,6 @@
 {
     nixpkgs = builtins.fetchGit {
-        url = "git@github.com:NixOS/nixpkgs.git";
+        url = "https://github.com/NixOS/nixpkgs";
         rev = "4b7664384c7600dadb4dc8d1708626d073681b40"; # 01-08-23
         ref = "master";
     };
