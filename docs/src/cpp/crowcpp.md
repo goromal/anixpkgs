553a6ffc3139dcfe71072a1f5066bbf3046ce5b2
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-07-12 23:13:09 -0700

    add more package docs

--- docs/src/cpp/crowcpp.md
+++ docs/src/cpp/crowcpp.md
@@ -1,3 +1,6 @@
 # crowcpp
 
-No package documentation currently provided.
+A minimally-patched [fork](https://github.com/goromal/Crow) of [Crow](https://github.com/CrowCpp/Crow), a C++ webserver.
+
+The patch allows one to dynamically specify where the website's assets directory is; a necessary feature for [rankserver-cpp](./rankserver-cpp.md).
+
--- pkgs/cxx-packages/crowcpp/default.nix
+++ pkgs/cxx-packages/crowcpp/default.nix
@@ -20,4 +20,10 @@ clangStdenv.mkDerivation {
     preConfigure = ''
     cmakeFlags="$cmakeFlags --no-warn-unused-cli"
     '';
+    meta = {
+        description = "A minimally-patched [fork](https://github.com/goromal/Crow) of [Crow](https://github.com/CrowCpp/Crow), a C++ webserver.";
+        longDescription = ''
+        The patch allows one to dynamically specify where the website's assets directory is; a necessary feature for [rankserver-cpp](./rankserver-cpp.md).
+        '';
+    };
 }
