2a15093ad4f04b6d0a2d6dedf195745900acd129
Andrew Torgesen <atorgesen@anduril.com>
2023-09-01 16:25:54 -0700

    add linting

---
+++ scripts/lint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e pipefail
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SOURCE="$(readlink "$SOURCE")"
+[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "$DIR/.."
+
+if [[ "$1" == "check" ]]; then
+    echo "Validating format of .nix files..."
+    for nixfile in $(find . -type f -name "*.nix"); do
+        echo "== $nixfile"
+        nixfmt -c "$nixfile"
+    done
+else
+    echo "Formatting .nix files..."
+    for nixfile in $(find . -type f -name "*.nix"); do 
+        nixfmt "$nixfile"
+    done
+fi
+echo "Done."
