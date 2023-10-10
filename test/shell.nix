b2e74123af3ec4b82d764e72f5d86198979d43c5
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-10-09 22:34:26 -0700

    add mp4unite to test env

--- test/shell.nix
+++ test/shell.nix
@@ -2,8 +2,15 @@ let pkgs = import ../default.nix { };
 in with pkgs;
 mkShell {
   nativeBuildInputs = [ git direnv lorri ];
-  buildInputs =
-    [ make-title devshell setupws listsources color-prints orchestrator ];
+  buildInputs = [
+    make-title
+    devshell
+    setupws
+    listsources
+    color-prints
+    mp4unite
+    orchestrator
+  ];
   shellHook = ''
     ${color-prints}/bin/echo_yellow "Entering anixpkgs test shell..."
   '';
