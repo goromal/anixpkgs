4023f8474191626a532d9e99602c9b499101a486
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-11-11 23:25:41 -0800

    finish home prep for test and deletion

--- pkgs/nixos/homes/personal.nix
+++ pkgs/nixos/homes/personal.nix
@@ -8,7 +8,10 @@ with import ../dependencies.nix { inherit config; }; {
   # TODO Nix config setup? e.g., cachix
   # TODO remove this and homes directory; just atomize the components packaging
 
-  imports = [ ../components/base-pkgs.nix ];
+  imports = [
+    ../components/base-pkgs.nix
+    ../components/x86-graphical-pkgs.nix
+  ];
 
   mods.x86-graphical.standalone = true;
   mods.x86-graphical.homeDir = "/home/andrew";
