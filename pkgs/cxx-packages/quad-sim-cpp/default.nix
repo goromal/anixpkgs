111aa038e770cbd7890a4d6a68cc32165ee7a1d0
Andrew Torgesen <atorgesen@anduril.com>
2023-11-26 18:12:15 -0800

    format

--- pkgs/cxx-packages/quad-sim-cpp/default.nix
+++ pkgs/cxx-packages/quad-sim-cpp/default.nix
@@ -1,25 +1,21 @@
-{ clangStdenv, cmake, eigen, manif-geom-cpp, signals-cpp, yaml-cpp, boost, pkg-src }:
+{ clangStdenv, cmake, eigen, manif-geom-cpp, signals-cpp, yaml-cpp, boost
+, pkg-src }:
 clangStdenv.mkDerivation {
   name = "quad-sim-cpp";
   version = "0.0.0";
   src = pkg-src;
   nativeBuildInputs = [ cmake ];
-  buildInputs = [
-    eigen
-    manif-geom-cpp
-    signals-cpp
-    boost
-    yaml-cpp
-  ];
+  buildInputs = [ eigen manif-geom-cpp signals-cpp boost yaml-cpp ];
   preConfigure = ''
     cmakeFlags="$cmakeFlags --no-warn-unused-cli"
   '';
   meta = {
-    description = "C++ library and daemon for simulating quadrotor dynamics from PWM motor inputs.";
+    description =
+      "C++ library and daemon for simulating quadrotor dynamics from PWM motor inputs.";
     longDescription = ''
-    [Repository](https://github.com/goromal/quad-sim-cpp)
+      [Repository](https://github.com/goromal/quad-sim-cpp)
 
-    ***Under construction.***
+      ***Under construction.***
     '';
   };
 }
