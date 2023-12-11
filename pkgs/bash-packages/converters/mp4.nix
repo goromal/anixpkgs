a702ba62158301f8e8d6986b0475da9069f36087
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-10 16:48:01 -0800

    add mp4 mute option (#167)

--- pkgs/bash-packages/converters/mp4.nix
+++ pkgs/bash-packages/converters/mp4.nix
@@ -18,6 +18,7 @@ let
 
     Options:
         -v | --verbose               Print verbose output from ffmpeg
+        -m | --mute                  Remove audio
         -q | --quality CHAR          - for low, = for medium, + for high bit rate quality
         -w | --width WIDTH           Constrain the video width (pixels)
         -l | --label "STR"           Add label to bottom left corner of video
@@ -33,6 +34,12 @@ let
       isBool = true;
       default = "0";
       flags = "-v|--verbose";
+    }
+    {
+      var = "mute";
+      isBool = true;
+      default = "0";
+      flags = "-m|--mute";
     }
     {
       var = "quality";
@@ -95,6 +102,12 @@ let
     fi
   '';
 
+  audioRule = ''
+    if [[ "$mute" == "1" ]]; then
+        ffmpeg_args+=( "-an" )
+    fi
+  '';
+
   widthRule = ''
     if [[ ! -z "$width" ]]; then
         ffmpeg_args+=( "-vf" "scale=$width:-1" )
@@ -131,6 +144,7 @@ let
     commands = ''
       ffmpeg_args=("-y" "-vcodec" "libx264")
       ${qualityRule}
+      ${audioRule}
       ${widthRule}
       ${labelRule}
       ${cropRule}
