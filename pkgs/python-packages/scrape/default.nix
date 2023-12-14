b57f0e57b6b1f1353f060ab4c0afa08592d276c5
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-13 22:25:48 -0800

    add scrape res to pages

--- pkgs/python-packages/scrape/default.nix
+++ pkgs/python-packages/scrape/default.nix
@@ -28,6 +28,22 @@ buildPythonPackage rec {
       -o DIRNAME, --output DIRNAME
                               Output directory.
       ```
+
+      **Resource files for testing scrape:**
+
+      [sample_1280x720](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_1280x720.webm)
+
+      [sample_1920x1080](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_1920x1080.webm)
+
+      [sample_2560x1440](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_2560x1440.webm)
+
+      [sample_3840x2160](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_3840x2160.webm)
+
+      [sample_640x360](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_640x360.webm)
+
+      [sample_960x400_ocean_with_audio](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_960x400_ocean_with_audio.webm)
+
+      [sample_960x540](https://github.com/goromal/anixdata/raw/master/data/media/scrape-tests/sample_960x540.webm)
     '';
   };
 }
