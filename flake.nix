7787abaaedabb9d5aa1a7bff21d72cd8ba8ed6e3
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-20 21:31:10 -0800

    add authm syncing; limit all auth refreshes to authm

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
     book-notes-sync.url = "github:goromal/book-notes-sync";
     book-notes-sync.flake = false;
 
-    easy-google-auth.url = "github:goromal/easy-google-auth";
+    easy-google-auth.url = "github:goromal/easy-google-auth?ref=dev/headless";
     easy-google-auth.flake = false;
 
     find_rotational_conventions.url =
