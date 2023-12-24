ae9b96708a3a1171422ddb4fb75a3317cb394c84
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-23 19:32:11 -0800

    wip

--- pkgs/nixos/components/ats-nixos-pkgs.nix
+++ pkgs/nixos/components/ats-nixos-pkgs.nix
@@ -3,18 +3,18 @@ with pkgs;
 with import ../dependencies.nix { inherit config; };
 let orchestratorPkg = anixpkgs.orchestrator;
 in {
-  imports = [
-    ../../python-packages/orchestrator/module.nix
-    ../../python-packages/ats-greeting/module.nix
-  ];
+  # imports = [
+  #   ../../python-packages/orchestrator/module.nix
+  #   ../../python-packages/ats-greeting/module.nix
+  # ];
 
-  services.orchestratord = {
-    enable = true;
-    inherit orchestratorPkg;
-  };
+  # services.orchestratord = {
+  #   enable = true;
+  #   inherit orchestratorPkg;
+  # };
 
-  services.ats-greeting = {
-    enable = true;
-    inherit orchestratorPkg;
-  };
+  # services.ats-greeting = {
+  #   enable = true;
+  #   inherit orchestratorPkg;
+  # };
 }
