365439e220a0df97813ce02723d67fc75f9d4fc0
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-24 22:26:37 -0800

    comment

--- pkgs/nixos/configurations/personal-inspiron.nix
+++ pkgs/nixos/configurations/personal-inspiron.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: {
   imports = [ ../profiles/personal.nix ../hardware/inspiron.nix ];
-  services.logind.lidSwitchExternalPower = "ignore";
+  # services.logind.lidSwitchExternalPower = "ignore"; TODO might not work
   networking.hostName = "atorgesen-inspiron";
 }
