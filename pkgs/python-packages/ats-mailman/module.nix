06905014b812284e68dfd298ac9c9d8a4797e39c
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-24 18:14:10 -0800

    Create module.nix

---
+++ pkgs/python-packages/ats-mailman/module.nix
@@ -0,0 +1,57 @@
+{ pkgs, config, lib, ... }:
+with pkgs;
+with lib;
+let
+  cfg = config.services.ats-mailman;
+  greetingScript = writeShellScript "ats-mailman" ''
+    set -e
+    authm refresh  || { >&2 echo "authm refresh error!"; exit 1; }
+    # TODO warn about expiration
+    goromail bot --headless
+    goromail journal --headless
+    if [[ ! -z "$(cat ${cfg.rootDir}/bot.log)" ]]; then
+      gmail-manager gbot-send 6612105214@vzwpix.com "ats-mailman" \
+        "[$(date)] Processed mail:\n$(cat ${cfg.rootDir}/bot.log)"
+    fi 
+    if [[ ! -z "$(cat ${cfg.rootDir}/journal.log)" ]]; then
+      gmail-manager gbot-send 6612105214@vzwpix.com "ats-mailman" \
+        "[$(date)] Processed mail:\n$(cat ${cfg.rootDir}/journal.log)"
+    fi
+  '';
+in {
+  options.services.ats-mailman = with types; {
+    enable = mkEnableOption "enable ATS mailman script";
+    rootDir = mkOption {
+      type = types.str;
+      description = "Root directory for data and configuration";
+      default = "/data/andrew/goromail";
+    };
+    orchestratorPkg = mkOption {
+      type = types.package;
+      description = "The orchestrator package to use";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.tmpfiles.rules =
+      [ "d  ${cfg.rootDir} - andrew dev" "Z  ${cfg.rootDir} - andrew dev" ];
+    systemd.services.ats-mailman = {
+      enable = true;
+      description = "ATS mailman script";
+      unitConfig = { StartLimitIntervalSec = 0; };
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${cfg.orchestratorPkg}/bin/orchestrator bash 'bash ${greetingScript}'";
+        Restart = "always";
+        RestartSec = 300;
+        ReadWritePaths = [ "${cfg.rootDir}" ];
+        WorkingDirectory = cfg.rootDir;
+        User = "andrew";
+        Group = "dev";
+      };
+      wantedBy = [ "multi-user.target" ];
+      after = [ "orchestratord.service" ];
+    };
+  };
+}
