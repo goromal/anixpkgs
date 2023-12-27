aa5778cf9ece97a2e2d310bf29f73913d5bc9a4e
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-27 08:33:33 -0800

    Add CI job for configuration build checks (#177)

---
+++ scripts/check_machines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+export NIXPKGS_ALLOW_UNFREE=1
+export NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1
+
+configurations=(personal-inspiron rankserver-pi)
+for configuration in ${configurations[@]}; do
+    echo "Checking derivation for configuration: $configuration..."
+    nix-build '<nixpkgs/nixos>' -A config.system.build.toplevel -I nixos-config=${DIR}/../pkgs/nixos/configurations/${configuration}.nix --no-out-link --dry-run
+    echo "Checks out!"
+    echo ""
+done
