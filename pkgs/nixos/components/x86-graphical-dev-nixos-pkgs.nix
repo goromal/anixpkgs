e7db38c115c50ac959ede053eda537602fdfa219
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-26 20:49:04 -0800

    lint

--- pkgs/nixos/components/x86-graphical-dev-nixos-pkgs.nix
+++ pkgs/nixos/components/x86-graphical-dev-nixos-pkgs.nix
@@ -4,8 +4,6 @@ with import ../dependencies.nix { inherit config; }; {
   imports = [ ./x86-graphical-dev-pkgs.nix ];
 
   dconf.settings = {
-    "org/gnome/shell" = {
-      "favorite-apps" = [ "codium.desktop" ];
-    };
+    "org/gnome/shell" = { "favorite-apps" = [ "codium.desktop" ]; };
   };
 }
