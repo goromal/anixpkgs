ec63709d88067dc575f94f9aaf53c0e97cbfb4f9
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-12-27 16:07:19 -0800

    Add flask logins

--- pkgs/python-packages/flasks/builders/mkSimpleFlaskApp.nix
+++ pkgs/python-packages/flasks/builders/mkSimpleFlaskApp.nix
@@ -57,16 +57,69 @@ let
     name = "${pname}.py";
     text = if !overrideFullFlaskScript then ''
       import flask
+      import flask_login
+      import flask_wtf
+      from wtforms import StringField, PasswordField, SubmitField
+      from werkzeug.security import generate_password_hash
       import argparse
+
+      class LoginForm(flask_wtf.FlaskForm):
+          username = StringField("Username")
+          password = PasswordField("Password")
+          submit = SubmitField("Submit")
+
+      class User:
+          def __init__(self):
+              self.is_authenticated = False
+              self.is_active = True
+              self.is_anonymous = True
+          def check_password(self, password):
+              return generate_password_hash(password) == "pbkdf2:sha256:260000$EuvRGQYWUOui9Y83$f70a1c1f22d4d590111f05e2ca462ef4eaac382bff39323bc5b6da6172b13957"
+          def get_id(self):
+              return "anonymous"
+
       app = flask.Flask(__name__)
+      app.secret_key = b"71d2dcdb895b367a1d5f0c66ca559c8d69af0c29a7e101c18c7c2d10399f264e"
+      login_manager = flask_login.LoginManager()
+
+      @login_manager.user_loader
+      def load_user(user_id):
+          if user_id == "anonymous":
+              return User()
+          else:
+              return None
+      
+      @app.route("/login", methods=["GET", "POST"])
+      def login():
+          if current_user.is_authenticated:
+              return redirect(url_for('index'))
+          form = LoginForm()
+          if form.validate_on_submit():
+              user = User()
+              if form.username.data != user.get_id() or not user.check_password(form.password.data):
+                  flask.flash("Invalid username or password")
+                  return redirect(flask.url_for("login"))
+              flask_login.login_user(user, remember=False)
+              return flask.redirect(flask.url_for("index"))
+          return render_template("login.html", title="Sign In", form=form)
+      
+      @app.route("/logout")
+      def logout():
+          flask_login.logout_user()
+          return flask.redirect(flask.url_for("index"))
+
       ${helper_script_pytxt}
       ${template_file_pytxt}
       ${flaskScript}
+
       def run():
           parser = argparse.ArgumentParser()
           parser.add_argument("--port", action="store", type=int, default=5000, help="Port to run the server on")
           args = parser.parse_args()
+          login_manager.init_app(app)
+          login_manager.login_view = "login"
           app.run(host="0.0.0.0", port=args.port)
+
       if __name__ == "__main__":
           run()
     '' else
