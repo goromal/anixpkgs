6e8723a493957b5ad94f656f490cfb6637dafe9c
Andrew Torgesen <atorgesen@anduril.com>
2024-01-04 22:18:29 -0800

    wip

--- pkgs/nixos/components/ats-standalone-home.nix
+++ pkgs/nixos/components/ats-standalone-home.nix
@@ -1,5 +1,8 @@
 { pkgs, config, lib, ... }:
 # TODO this is a very bespoke script--adapt or use sparingly
+# probably replace the standalone-modules/ and orchestrator/ modules with these home-manager versions
+# in a components/ats.nix file (i.e., this file with added configurability, removal of home-manager configs,
+# and accommodation of NixOS paths for e.g., coreutils)
 with pkgs;
 with import ../dependencies.nix { inherit config; };
 with anixpkgs;
@@ -17,6 +20,31 @@ let
       "[$(date)] 🌞 Hello, world! I'm awake! authm refreshed successfully ✅"
     gmail-manager gbot-send andrew.torgesen@gmail.com "ats-greeting" \
       "[$(date)] 🌞 Hello, world! I'm awake! authm refreshed successfully ✅"
+  '';
+  counterScript = writeShellScript "ats-ccounterd" ''
+    authm refresh --headless 1  || { >&2 echo "authm refresh error!"; exit 1; }
+    lines="$(wiki-tools get --page-id calorie-journal | grep $(printf '%(%Y-%m-%d)T\n' -1))"
+    if [[ -z $lines ]]; then
+      exit
+    fi
+    ctotal=0
+    clim=1950
+    while IFS= read -r line; do
+      c=`echo ''${line##* }`
+      ctotal=$(( ctotal + c ))
+    done <<< "$lines"
+    echo $ctotal
+    if (( ctotal <= clim )); then
+      gmail-manager gbot-send 6612105214@vzwpix.com "ats-ccounterd" \
+        "[$(date)] 🗒️ Calorie counter: $ctotal / $clim ✅"
+      gmail-manager gbot-send andrew.torgesen@gmail.com "ats-ccounterd" \
+        "[$(date)] 🗒️ Calorie counter: $ctotal / $clim ✅"
+    else
+      gmail-manager gbot-send 6612105214@vzwpix.com "ats-ccounterd" \
+        "[$(date)] 🗒️ Calorie counter: $ctotal / $clim - Watch out! 🚨"
+      gmail-manager gbot-send andrew.torgesen@gmail.com "ats-ccounterd" \
+        "[$(date)] 🗒️ Calorie counter: $ctotal / $clim - Watch out! 🚨"
+    fi
   '';
 in {
   home.username = "andrew";
@@ -46,6 +74,7 @@ in {
     };
     Timer = {
       OnBootSec = "1m";
+      Persistent = true;
       Unit = "ats-greeting.service";
     };
     Install.WantedBy = [ "timers.target" ];
@@ -61,5 +90,26 @@ in {
       ReadWritePaths = [ "/home/andrew" ];
     };
     Install.WantedBy = [ "default.target" ];
+  };
+  # TODO ats-mailman
+  systemd.user.timers.ats-ccounterd = {
+    Unit = {
+      Description = "ATS ccounterd timer";
+    };
+    Timer = {
+      OnCalendar = [ "*-*-* 10:00:00" "*-*-* 14:00:00" "*-*-* 20:00:00" ];
+      # triggers the service immediately if it missed the last start time
+      Persistent = true;
+      Unit = "ats-ccounterd.service";
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
+  systemd.user.services.ats-ccounterd = {
+    Unit.Description = "ATS ccounterd script";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      Type = "oneshot";
+      ReadWritePaths = [ "/home/andrew" ];
+    };
   };
 }
