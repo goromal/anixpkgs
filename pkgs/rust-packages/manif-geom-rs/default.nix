b7c4a144079efb48cbe2e03632b1cdebe85106e6
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-01-14 21:15:59 -0800

    Break out rust, java, bash packages; add manif-geom-rs so2 impl

--- pkgs/rust-packages/manif-geom-rs/default.nix
+++ pkgs/rust-packages/manif-geom-rs/default.nix
@@ -3,7 +3,7 @@ rustPlatform.buildRustPackage rec {
   pname = "manif-geom-rs";
   version = "0.0.0";
   src = pkg-src;
-  cargoHash = "sha256-ceayumOzXbEFi2j42BIXXo9L3r/VFgUyfNUuk3vhYu8=";
+  cargoHash = "";
   meta = {
     description =
       "Rust implementation of [manif-geom-cpp](https://github.com/goromal/manif-geom-cpp) (*under construction*).";
