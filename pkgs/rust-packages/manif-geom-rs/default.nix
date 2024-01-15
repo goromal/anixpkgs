ab9631f7d48048e5a680cb7c8fa0c2daf815b492
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-01-14 21:19:14 -0800

    Update default.nix

--- pkgs/rust-packages/manif-geom-rs/default.nix
+++ pkgs/rust-packages/manif-geom-rs/default.nix
@@ -3,7 +3,7 @@ rustPlatform.buildRustPackage rec {
   pname = "manif-geom-rs";
   version = "0.0.0";
   src = pkg-src;
-  cargoHash = "";
+  cargoHash = "sha256-KbAGQ8V3hWx2i69d/JswkzaQSwizQUBzvPDaGGE6PBU=";
   meta = {
     description =
       "Rust implementation of [manif-geom-cpp](https://github.com/goromal/manif-geom-cpp) (*under construction*).";
