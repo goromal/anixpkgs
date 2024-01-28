75b861e6450d6f503279e55905edaa69e6ea4aba
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-01-28 11:10:07 -0800

    declarative nautilus bookmarks

--- pkgs/nixos/components/x86-graphical-nixos-pkgs.nix
+++ pkgs/nixos/components/x86-graphical-nixos-pkgs.nix
@@ -90,5 +90,9 @@ in {
       themes.nordic-gtk4.css-dark.data;
     ".config/gtk-4.0/${themes.nordic-gtk4.thumbnail.name}".source =
       themes.nordic-gtk4.thumbnail.data;
+    ".config/gtk-4.0/bookmarks".text = ''
+      file:///data/andrew/dev Development
+      file:///data/andrew/data Data
+    '';
   };
 }
