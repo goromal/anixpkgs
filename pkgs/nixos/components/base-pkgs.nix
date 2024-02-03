b219a3ae7d0688321ad7bedbd31ce28017c67b23
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-02-03 14:32:07 -0800

    standardize opts

--- pkgs/nixos/components/base-pkgs.nix
+++ pkgs/nixos/components/base-pkgs.nix
@@ -1,82 +1,99 @@
 { pkgs, config, lib, ... }:
 with pkgs;
 with import ../dependencies.nix { inherit config; }; {
-  # TODO temporary fix for bad URL dependency in home-manager
-  manual.manpages.enable = false;
-
-  home.packages = [
-    rclone
-    anixpkgs.authm
-    anixpkgs.rcrsync
-    anixpkgs.goromail
-    anixpkgs.manage-gmail
-    anixpkgs.gmail-parser
-    anixpkgs.wiki-tools
-    anixpkgs.book-notes-sync
-    anixpkgs.color-prints
-    anixpkgs.fix-perms
-    anixpkgs.secure-delete
-    anixpkgs.sunnyside
-    anixpkgs.make-title
-    anixpkgs.pb
-    anixpkgs.dirgroups
-    anixpkgs.fixfname
-    anixpkgs.nix-deps
-    anixpkgs.nix-diffs
-    anixpkgs.orchestrator
-    anixpkgs.rankserver-cpp
-    anixpkgs.stampserver
-    anixpkgs.gantter
-    anixpkgs.md2pdf
-    anixpkgs.notabilify
-    anixpkgs.code2pdf
-    anixpkgs.abc
-    anixpkgs.doku
-    anixpkgs.epub
-    anixpkgs.gif
-    anixpkgs.md
-    anixpkgs.mp3
-    anixpkgs.mp4
-    anixpkgs.mp4unite
-    anixpkgs.pdf
-    anixpkgs.png
-    anixpkgs.svg
-    anixpkgs.zipper
-    anixpkgs.scrape
-  ];
-
-  programs.vim = {
-    enable = true;
-    extraConfig = ''
-      if has('gui_running')
-          set guifont=Iosevka
-      endif
-      set expandtab
-      " open NERDTree automatically if no file specified
-      "autocmd StdinReadPre * let s:std_in=1
-      "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
-      " open NERDTree on Ctrl-n
-      map <C-n> :NERDTreeToggle<CR>
-      set wildignore+=*/node_modules/*,_site,*/__pycache__/,*/venv/*,*/target/*,*/.vim$,\~$,*/.log,*/.aux,*/.cls,*/.aux,*/.bbl,*/.blg,*/.fls,*/.fdb*/,*/.toc,*/.out,*/.glo,*/.log,*/.ist,*/.fdb_latexmk
-      set encoding=utf-8
-      set termguicolors
-      set background=dark
-      let g:mix_format_on_save = 1
-      let g:mix_format_options = '--check-equivalent'
-    '';
-    settings = { number = true; };
-    plugins = with vimPlugins; [
-      vim-elixir
-      sensible
-      vim-airline
-      The_NERD_tree
-      fugitive
-      vim-abolish
-      command-t
-    ];
+  options.mods.base = {
+    standalone = lib.mkOption {
+      type = lib.types.bool;
+      description =
+        "Whether this is a standalone Nix installation (default: false)";
+      default = false;
+    };
+    homeDir = lib.mkOption {
+      type = lib.types.str;
+      description =
+        "Home directory to put the wallpaper in (default: /data/andrew)";
+      default = "/data/andrew";
+    };
   };
 
-  home.file = with anixpkgs.pkgData; {
-    "records/${records.crypt.name}".source = records.crypt.data;
+  config = {
+    # TODO temporary fix for bad URL dependency in home-manager (23.05)
+    manual.manpages.enable = false;
+
+    home.packages = [
+      rclone
+      anixpkgs.authm
+      anixpkgs.rcrsync
+      anixpkgs.goromail
+      anixpkgs.manage-gmail
+      anixpkgs.gmail-parser
+      anixpkgs.wiki-tools
+      anixpkgs.book-notes-sync
+      anixpkgs.color-prints
+      anixpkgs.fix-perms
+      anixpkgs.secure-delete
+      anixpkgs.sunnyside
+      anixpkgs.make-title
+      anixpkgs.pb
+      anixpkgs.dirgroups
+      anixpkgs.fixfname
+      anixpkgs.nix-deps
+      anixpkgs.nix-diffs
+      anixpkgs.orchestrator
+      anixpkgs.rankserver-cpp
+      anixpkgs.stampserver
+      anixpkgs.gantter
+      anixpkgs.md2pdf
+      anixpkgs.notabilify
+      anixpkgs.code2pdf
+      anixpkgs.abc
+      anixpkgs.doku
+      anixpkgs.epub
+      anixpkgs.gif
+      anixpkgs.md
+      anixpkgs.mp3
+      anixpkgs.mp4
+      anixpkgs.mp4unite
+      anixpkgs.pdf
+      anixpkgs.png
+      anixpkgs.svg
+      anixpkgs.zipper
+      anixpkgs.scrape
+    ];
+
+    programs.vim = {
+      enable = true;
+      extraConfig = ''
+        if has('gui_running')
+            set guifont=Iosevka
+        endif
+        set expandtab
+        " open NERDTree automatically if no file specified
+        "autocmd StdinReadPre * let s:std_in=1
+        "autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif
+        " open NERDTree on Ctrl-n
+        map <C-n> :NERDTreeToggle<CR>
+        set wildignore+=*/node_modules/*,_site,*/__pycache__/,*/venv/*,*/target/*,*/.vim$,\~$,*/.log,*/.aux,*/.cls,*/.aux,*/.bbl,*/.blg,*/.fls,*/.fdb*/,*/.toc,*/.out,*/.glo,*/.log,*/.ist,*/.fdb_latexmk
+        set encoding=utf-8
+        set termguicolors
+        set background=dark
+        let g:mix_format_on_save = 1
+        let g:mix_format_options = '--check-equivalent'
+      '';
+      settings = { number = true; };
+      plugins = with vimPlugins; [
+        vim-elixir
+        sensible
+        vim-airline
+        The_NERD_tree
+        fugitive
+        vim-abolish
+        command-t
+      ];
+    };
+
+    home.file = with anixpkgs.pkgData; {
+      "records/${records.crypt.name}".source = records.crypt.data;
+    };
   };
 }
--- pkgs/nixos/components/x86-graphical-pkgs.nix
+++ pkgs/nixos/components/x86-graphical-pkgs.nix
@@ -1,59 +1,42 @@
 { pkgs, config, lib, ... }:
 with pkgs;
 with import ../dependencies.nix { inherit config; };
-let cfg = config.mods.x86-graphical;
+let cfg = config.mods.base;
 in {
-  options.mods.x86-graphical = {
-    standalone = lib.mkOption {
-      type = lib.types.bool;
-      description =
-        "Whether this is a standalone Nix installation (default: false)";
-      default = false;
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      "picture-uri" = "${cfg.homeDir}/.background-image";
     };
-    homeDir = lib.mkOption {
-      type = lib.types.str;
-      description =
-        "Home directory to put the wallpaper in (default: /data/andrew)";
-      default = "/data/andrew";
+    "org/gnome/desktop/screensaver" = {
+      "picture-uri" = "${cfg.homeDir}/.background-image";
     };
   };
 
-  config = {
-    dconf.settings = {
-      "org/gnome/desktop/background" = {
-        "picture-uri" = "${cfg.homeDir}/.background-image";
-      };
-      "org/gnome/desktop/screensaver" = {
-        "picture-uri" = "${cfg.homeDir}/.background-image";
-      };
-    };
+  home.packages = [ terminator anixpkgs.budget_report ];
 
-    home.packages = [ terminator anixpkgs.budget_report ];
-
-    home.file = with anixpkgs.pkgData; {
-      # TODO the TK_LIBRARY hack should only be necessary until we move on from 23.05;
-      # see https://github.com/NixOS/nixpkgs/issues/234962
-      "TK_LIB_VARS.sh".text = ''
-        export TK_LIBRARY="${pkgs.tk}/lib/${pkgs.tk.libPrefix}"
-      '';
-      ".config/terminator/config".source =
-        ../res/terminator-config; # https://rigel.netlify.app/#terminal
-      ".local/share/nautilus/scripts/terminal".source =
-        (writeShellScript "terminal" "terminator");
-      ".config/nautilus/scripts-accels".text = "F4 terminal";
-      "Templates/EmptyDocument".text = "";
-      ".background-image".source = ((runCommand "make-wallpaper" { } ''
-        mkdir $out
-        ${imagemagick}/bin/convert -font ${fonts.nexa.data} \
-           -pointsize 30 \
-           -fill black \
-           -draw 'text 320,1343 "${
-             if local-build then "Local Build" else "v${anix-version}"
-           } - ${
-             if cfg.standalone then "Home-Manager" else "NixOS"
-           } ${nixos-version}"' \
-           ${img.wallpaper.data} $out/wallpaper.png
-      '') + "/wallpaper.png");
-    };
+  home.file = with anixpkgs.pkgData; {
+    # TODO the TK_LIBRARY hack should only be necessary until we move on from 23.05;
+    # see https://github.com/NixOS/nixpkgs/issues/234962
+    "TK_LIB_VARS.sh".text = ''
+      export TK_LIBRARY="${pkgs.tk}/lib/${pkgs.tk.libPrefix}"
+    '';
+    ".config/terminator/config".source =
+      ../res/terminator-config; # https://rigel.netlify.app/#terminal
+    ".local/share/nautilus/scripts/terminal".source =
+      (writeShellScript "terminal" "terminator");
+    ".config/nautilus/scripts-accels".text = "F4 terminal";
+    "Templates/EmptyDocument".text = "";
+    ".background-image".source = ((runCommand "make-wallpaper" { } ''
+      mkdir $out
+      ${imagemagick}/bin/convert -font ${fonts.nexa.data} \
+         -pointsize 30 \
+         -fill black \
+         -draw 'text 320,1343 "${
+           if local-build then "Local Build" else "v${anix-version}"
+         } - ${
+           if cfg.standalone then "Home-Manager" else "NixOS"
+         } ${nixos-version}"' \
+         ${img.wallpaper.data} $out/wallpaper.png
+    '') + "/wallpaper.png");
   };
 }
