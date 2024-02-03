0fa52ad424a32c8b98438ab358404e6215e5c551
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-02-03 09:27:25 -0800

    Add flask login to stampserver (#179)

--- pkgs/python-packages/flasks/stampserver/default.nix
+++ pkgs/python-packages/flasks/stampserver/default.nix
@@ -1,22 +1,28 @@
-{ buildPythonPackage, flask, strings, writeTextFile, callPackage
+{ buildPythonPackage, flask, flask_login, flask_wtf, wtforms, werkzeug
 , writeShellScript, python }:
-callPackage ../builders/mkSimpleFlaskApp.nix {
+let pythonLibDir = "lib/python${python.passthru.pythonVersion}/site-packages";
+in buildPythonPackage rec {
   pname = "stampserver";
   version = "0.0.0";
-  inherit buildPythonPackage flask writeTextFile writeShellScript python;
-  scriptPropagatedBuildInputs = [ ];
-  overrideFullFlaskScript = true;
-  flaskScript = builtins.readFile ./server.py;
-  templateText = builtins.readFile ./index.html;
-  description = "Provides an interface for stamping metadata on PNGs and MP4s.";
-  longDescription = ''
-    ```bash
-    usage: stampserver [-h] [--port PORT] [--data-dir DATA_DIR]
-
-    optional arguments:
-      -h, --help           show this help message and exit
-      --port PORT          Port to run the server on
-      --data-dir DATA_DIR  Directory containing the stampable elements
-    ```
+  src = ./.;
+  prePatch = ''
+    mkdir -p $out/${pythonLibDir}/templates
+    cp ${./index.html} $out/${pythonLibDir}/templates/index.html
+    cp ${./login.html} $out/${pythonLibDir}/templates/login.html
   '';
+  propagatedBuildInputs = [ flask flask_login flask_wtf wtforms werkzeug ];
+  meta = {
+    description =
+      "Provides an interface for stamping metadata on PNGs and MP4s.";
+    longDescription = ''
+      ```bash
+      usage: stampserver [-h] [--port PORT] [--data-dir DATA_DIR]
+
+      optional arguments:
+        -h, --help           show this help message and exit
+        --port PORT          Port to run the server on
+        --data-dir DATA_DIR  Directory containing the stampable elements
+      ```
+    '';
+  };
 }
