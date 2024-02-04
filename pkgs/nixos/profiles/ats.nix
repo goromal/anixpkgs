3fb0e2376d81969d19f902c21b2e792d3855d4b8
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-02-03 21:30:49 -0800

    Fix ats-standalone-home.nix (#204)

--- pkgs/nixos/profiles/ats.nix
+++ pkgs/nixos/profiles/ats.nix
@@ -9,4 +9,13 @@
     isServer = true;
     isInstaller = false;
   };
+
+  # TODO for now ATS needs to be founded on home-manager;
+  # most of the content below is filler boilerplate for
+  # ats-standalone-home.nix (for installer testing purposes ONLY)
+
+  home-manager.users.andrew = {
+    home.homeDirectory = pkgs.lib.mkForce "/home/andrew";
+    imports = [ ../components/ats-standalone-home.nix ];
+  };
 }
