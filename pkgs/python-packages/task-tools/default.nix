e58ea5f333617dfe768aa3c113ae08740ef3acb5
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-02-25 13:16:38 -0800

    Update default.nix

--- pkgs/python-packages/task-tools/default.nix
+++ pkgs/python-packages/task-tools/default.nix
@@ -1,10 +1,11 @@
-{ buildPythonPackage, click, easy-google-auth, pkg-src }:
+{ buildPythonPackage, pytestCheckHook, click, easy-google-auth, pkg-src }:
 buildPythonPackage rec {
   pname = "task-tools";
   version = "0.0.0";
   src = pkg-src;
   propagatedBuildInputs = [ click easy-google-auth ];
   doCheck = true;
+  checkInputs = [ pytestCheckHook ];
   meta = {
     description = "CLI tools for managing Google Tasks.";
     longDescription = ''
