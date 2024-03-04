69c9e8e2f4ad267c03632a0a9350d59b7cd979d0
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-03-04 10:52:00 -0800

    Update ats-standalone-home.nix

--- pkgs/nixos/components/ats-standalone-home.nix
+++ pkgs/nixos/components/ats-standalone-home.nix
@@ -170,7 +170,7 @@ in {
   imports =
     [ ./opts.nix ./base-pkgs.nix ./x86-graphical-pkgs.nix ./x86-rec-pkgs.nix ];
   mods.opts.standalone = lib.mkForce true;
-  mods.opts.homeDir = "/home/andrew";
+  mods.opts.homeDir = lib.mkForce "/home/andrew";
   home.packages = [
     (writeShellScriptBin "ats-load-services" ''
       mkdir -p /home/andrew/goromail
