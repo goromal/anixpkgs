1cc83160e409f93e0e1846acecf503fa38579c8c
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-03-24 21:50:20 -0700

    Add user cloud sync jobs via orchestrator on all machines (#229)

---
+++ scripts/check_deps.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+ANIXDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+with open(os.path.join(ANIXDIR, "flake.lock"), "r") as lockfile:
+    lock = json.loads(lockfile.read())
+
+for src in lock["nodes"]:
+    if "original" in lock["nodes"][src]:
+        original = lock["nodes"][src]["original"]
+        if "ref" in original and "refs/" not in original["ref"]:
+            print(f"Source {src} is checked out on a side branch: {original['ref']}")
