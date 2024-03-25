31490c79c6c6408a5323ecdbd1f67e7f89de9137
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-03-24 21:38:34 -0700

    merge deps

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     anixdata.url = "github:goromal/anixdata";
     anixdata.flake = false;
 
-    aapis.url = "github:goromal/aapis?ref=dev/sync";
+    aapis.url = "github:goromal/aapis";
     aapis.flake = false;
 
     ceres-factors.url = "github:goromal/ceres-factors";
@@ -84,7 +84,7 @@
     mesh-plotter.url = "github:goromal/mesh-plotter";
     mesh-plotter.flake = false;
 
-    orchestrator.url = "github:goromal/orchestrator?ref=dev/sync";
+    orchestrator.url = "github:goromal/orchestrator";
     orchestrator.flake = false;
 
     pyceres.url =
