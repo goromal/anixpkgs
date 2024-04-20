57b75dc94ed8f1c36a8d97e74e60a791ceb6e9c5
Andrew Torgesen <atorgesen@anduril.com>
2024-04-19 23:10:33 -0700

    test passing

--- pkgs/bash-packages/converters/mkConverter.nix
+++ pkgs/bash-packages/converters/mkConverter.nix
@@ -9,7 +9,7 @@ let
     ;;
   '') convOptCmds;
   conv_opt_cmds = builtins.concatStringsSep "\n" conv_opt_list;
-  printerr = "${color-prints}/bin/echo_red";
+  printerr = ">&2 ${color-prints}/bin/echo_red";
   printusage = ''
         cat << EOF
     ${usage_str}
@@ -20,13 +20,13 @@ in (writeArgparseScriptBin name usage_str optsWithVarsAndDefaults ''
   if [[ -z "$infile" ]]; then
       ${printerr} "ERROR: no input file specified."
       ${printusage}
-      exit
+      exit 1
   fi
   infile_ext=`${strings.getExtension} "$infile"`
   if [[ -z "$2" ]]; then
       ${printerr} "ERROR: no output file specified."
       ${printusage}
-      exit
+      exit 1
   fi
   outfile=`${strings.replaceExtension} "$2" ${extension}`
   case $infile_ext in
@@ -34,7 +34,7 @@ in (writeArgparseScriptBin name usage_str optsWithVarsAndDefaults ''
   *)
   ${printerr} "ERROR: unhandled input extension ($infile_ext)."
   ${printusage}
-  exit
+  exit 1
   ;;
   esac
 '') // {
--- test/test.sh
+++ test/test.sh
@@ -106,26 +106,26 @@ done
 
 echo "Spawning server with $num_server_threads threads"
 
-nohup orchestratord -n $num_server_threads > /dev/null 2>&1 &
+nohup orchestratord -p 5555 -n $num_server_threads > /dev/null 2>&1 &
 serverPID=$!
 
 sleep 4
 
 echo "Spawning jobs"
 
-rmjob=$(orchestrator remove $orchoutpath/sample_960x400_ocean_with_audio.webm)
-rmjob=$(orchestrator remove -b $rmjob $orchoutpath/sample_1280x720.webm)
-rmjob=$(orchestrator remove -b $rmjob $orchoutpath/sample_1920x1080.webm)
-rmjob=$(orchestrator remove -b $rmjob $orchoutpath/sample_2560x1440.webm)
-rmjob=$(orchestrator remove -b $rmjob $orchoutpath/sample_3840x2160.webm)
-lsjob=$(orchestrator listing -b $rmjob --ext webm $orchoutpath)
-mp4job=$(orchestrator mp4 $lsjob $orchoutpath/vid.mp4)
-rmjob=$(orchestrator remove $lsjob -b $mp4job)
-unijob=$(orchestrator mp4-unite $mp4job $orchoutpath/unified_vid.mp4)
-rmjob=$(orchestrator remove $mp4job -b $unijob)
+rmjob=$(orchestrator -p 5555 remove $orchoutpath/sample_960x400_ocean_with_audio.webm)
+rmjob=$(orchestrator -p 5555 remove -b $rmjob $orchoutpath/sample_1280x720.webm)
+rmjob=$(orchestrator -p 5555 remove -b $rmjob $orchoutpath/sample_1920x1080.webm)
+rmjob=$(orchestrator -p 5555 remove -b $rmjob $orchoutpath/sample_2560x1440.webm)
+rmjob=$(orchestrator -p 5555 remove -b $rmjob $orchoutpath/sample_3840x2160.webm)
+lsjob=$(orchestrator -p 5555 listing -b $rmjob --ext webm $orchoutpath)
+mp4job=$(orchestrator -p 5555 mp4 $lsjob $orchoutpath/vid.mp4)
+rmjob=$(orchestrator -p 5555 remove $lsjob -b $mp4job)
+unijob=$(orchestrator -p 5555 mp4-unite $mp4job $orchoutpath/unified_vid.mp4)
+rmjob=$(orchestrator -p 5555 remove $mp4job -b $unijob)
 
 echo "touch $orchoutpath/new.txt" > "$tmpdir/touchfile.sh"
-bjob=$(orchestrator bash "bash $tmpdir/touchfile.sh")
+bjob=$(orchestrator -p 5555 bash "bash $tmpdir/touchfile.sh")
 
 num_pending=1
 timeout_secs=60
@@ -134,7 +134,7 @@ num_tries=0
 echo "Waiting for pending jobs..."
 
 while (( num_pending > 0 )) && (( num_tries < timeout_secs )); do
-    num_pending=$(orchestrator status count-pending)
+    num_pending=$(orchestrator -p 5555 status count-pending)
     echo "Filesystem: ($num_pending)"
     ls $orchoutpath
     echo "----------------"
@@ -144,15 +144,27 @@ done
 
 if [ $num_pending -ne 0 ]; then
     echo_red "ERROR: orchestrator timed out at $timeout_secs seconds with $num_pending unfinished jobs:"
-    orchestrator status all
-    echo "----"
-    ls $orchoutpath
-    echo "----"
+    for jid in $(orchestrator -p 5555 status get-pending); do
+        orchestrator -p 5555 status $jid
+    done
     kill $serverPID
     exit 1
 fi
 
 echo "All jobs complete at $num_tries seconds"
+for jid in $(orchestrator -p 5555 status get-complete); do
+    orchestrator -p 5555 status $jid
+done
+
+num_discarded=$(orchestrator -p 5555 status count-discarded)
+if [ $num_discarded -ne 0 ]; then
+    echo_red "ERROR: orchestrator finished with $num_discarded discarded jobs:"
+    for jid in $(orchestrator -p 5555 status get-discarded); do
+        orchestrator -p 5555 status $jid
+    done
+    kill $serverPID
+    exit 1
+fi
 
 if [ ! -f "$orchoutpath/unified_vid.mp4" ]; then
     echo_red "ERROR: expected workflow output video not present"
