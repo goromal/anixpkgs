8e0a3a19863ea7a36c4320c95b9c25f361c4533c
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-05-29 16:37:31 -0700

    Google secrets consolidation (#249)

--- pkgs/bash-packages/authm/authm.py
+++ pkgs/bash-packages/authm/authm.py
@@ -26,75 +26,44 @@ def cli():
 )
 def refresh(headless, force):
     """Refresh all auth tokens one-by-one."""
-    print(Fore.YELLOW + "Refreshing GMail token (personal)..." + Style.RESET_ALL)
+    print(Fore.YELLOW + "Refreshing Personal Tokens..." + Style.RESET_ALL)
     try:
         getGoogleService(
             "gmail",
             "v1",
             GPD.getKwargsOrDefault("gmail_secrets_json"),
             GPD.getKwargsOrDefault("gmail_refresh_file"),
-            GPD.getKwargsOrDefault("gmail_corpus_scope"),
             headless=headless,
             force=force,
         )
     except CredentialsRefreshException:
         sys.stderr.write(f"Refresh of {GPD.getKwargsOrDefault('gmail_refresh_file')} needed.")
         exit(1)
-    print(Fore.YELLOW + "Refreshing GMail token (gbot)..." + Style.RESET_ALL)
+    print(Fore.YELLOW + "Refreshing Bot Tokens..." + Style.RESET_ALL)
     try:
         getGoogleService(
             "gmail",
             "v1",
             GPD.getKwargsOrDefault("gmail_secrets_json"),
             GPD.getKwargsOrDefault("gbot_refresh_file"),
-            GPD.getKwargsOrDefault("gmail_corpus_scope"),
             headless=headless,
             force=force,
         )
     except CredentialsRefreshException:
         sys.stderr.write(f"Refresh of {GPD.getKwargsOrDefault('gbot_refresh_file')} needed.")
         exit(1)
-    print(Fore.YELLOW + "Refreshing GMail token (journal)..." + Style.RESET_ALL)
+    print(Fore.YELLOW + "Refreshing Journal Tokens..." + Style.RESET_ALL)
     try:
         getGoogleService(
             "gmail",
             "v1",
             GPD.getKwargsOrDefault("gmail_secrets_json"),
             GPD.getKwargsOrDefault("journal_refresh_file"),
-            GPD.getKwargsOrDefault("gmail_corpus_scope"),
             headless=headless,
             force=force,
         )
     except CredentialsRefreshException:
         sys.stderr.write(f"Refresh of {GPD.getKwargsOrDefault('journal_refresh_file')} needed.")
-        exit(1)
-    print(Fore.YELLOW + "Refreshing Docs token..." + Style.RESET_ALL)
-    try:
-        getGoogleService(
-            "docs",
-            "v1",
-            BNSD.getKwargsOrDefault("docs_secrets_file"),
-            BNSD.getKwargsOrDefault("docs_refresh_token"),
-            BNSD.getKwargsOrDefault("docs_scope"),
-            headless=headless,
-            force=force,
-        )
-    except CredentialsRefreshException:
-        sys.stderr.write(f"Refresh of {BNSD.getKwargsOrDefault('docs_refresh_token')} needed.")
-        exit(1)
-    print(Fore.YELLOW + "Refreshing Tasks token..." + Style.RESET_ALL)
-    try:
-        getGoogleService(
-            "tasks",
-            "v1",
-            TTD.getKwargsOrDefault("task_secrets_file"),
-            TTD.getKwargsOrDefault("task_refresh_token"),
-            TTD.getKwargsOrDefault("task_scope"),
-            headless=headless,
-            force=force,
-        )
-    except CredentialsRefreshException:
-        sys.stderr.write(f"Refresh of {TTD.getKwargsOrDefault('task_refresh_token')} needed.")
         exit(1)
     print(Fore.GREEN + "DONE" + Style.RESET_ALL)
 
