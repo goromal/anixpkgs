cbaba85189b74c0a572e6d243094d6839c76f742
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-06-04 23:43:53 -0700

    rust-helper: add drop-in shell option; anixpkgs-version cleanup (#253)

--- pkgs/nixos/dependencies.nix
+++ pkgs/nixos/dependencies.nix
@@ -1,18 +1,18 @@
 { config }:
 let
   nixos-version = (builtins.readFile ../../NIXOS_VERSION);
-  anix-version = (builtins.readFile ../../ANIX_VERSION);
+  anixpkgs-version = (builtins.readFile ../../ANIX_VERSION);
 in rec {
   local-build = false;
   inherit nixos-version; # Should match the channel in <nixpkgs>
   nixos-state = nixos-version;
   homem-state = nixos-version;
-  inherit anix-version;
+  inherit anixpkgs-version;
   anixpkgs = import (if local-build then
     ../../default.nix
   else
     (builtins.fetchTarball
-      "https://github.com/goromal/anixpkgs/archive/refs/tags/v${anix-version}.tar.gz"))
+      "https://github.com/goromal/anixpkgs/archive/refs/tags/v${anixpkgs-version}.tar.gz"))
     { };
   unstable = import (builtins.fetchTarball
     "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
