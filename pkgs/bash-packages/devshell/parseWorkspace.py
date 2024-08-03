fe283393ec359bcfd4fbb7d862d9fb5b01a3116b
Andrew Torgesen <atorgesen@anduril.com>
2024-08-02 22:03:44 -0700

    reg tests

--- pkgs/bash-packages/devshell/parseWorkspace.py
+++ pkgs/bash-packages/devshell/parseWorkspace.py
@@ -49,7 +49,7 @@ def get_deps_from_dep(dep, attr):
         linesplit = deps_line.split()
         if len(linesplit) > 1:
             for dep in linesplit:
-                deps.append(f'"{dep}"')
+                deps.append(f'{dep}')
         else:
             deps.append(f'"{linesplit[0]}"')
     return deps
