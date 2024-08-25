003a6888bcdb91c117eea1c3e283efcf77bf4055
Andrew Torgesen <atorgesen@anduril.com>
2024-08-25 14:03:45 -0700

    browser

--- pkgs/nixos/components/x86-graphical-pkgs.nix
+++ pkgs/nixos/components/x86-graphical-pkgs.nix
@@ -49,7 +49,7 @@ in {
   } else
     { }));
 
-  home.packages = [ terminator anixpkgs.getres ]
+  home.packages = [ sqlitebrowser terminator anixpkgs.getres ]
     ++ (if cfg.standalone == false then [
       kooha # wayland-compatible screen recorder
       gnome3.gnome-tweaks
