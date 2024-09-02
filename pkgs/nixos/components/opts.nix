1eb84b8986f15e74eef119787f0e7ce981c9ea40
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-09-02 08:15:56 -0700

    wip

--- pkgs/nixos/components/opts.nix
+++ pkgs/nixos/components/opts.nix
@@ -1,43 +1,44 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, ... }:
 with pkgs;
+with lib;
 with import ../dependencies.nix { inherit config; };
 let opts = config.mods.opts;
 in {
   options.mods.opts = {
-    standalone = lib.mkOption {
-      type = lib.types.bool;
+    standalone = mkOption {
+      type = types.bool;
       description =
         "Whether this is a standalone Nix installation (default: false)";
       default = false;
     };
-    homeDir = lib.mkOption {
-      type = lib.types.str;
+    homeDir = mkOption {
+      type = types.str;
       description =
         "Home directory to put the wallpaper in (default: /data/andrew)";
       default = "/data/andrew";
     };
-    homeState = lib.mkOption {
-      type = lib.types.str;
+    homeState = mkOption {
+      type = types.str;
       description = "Initiating state of home-manager (example: '22.05')";
     };
-    userOrchestrator = lib.mkOption {
-      type = lib.types.bool;
+    userOrchestrator = mkOption {
+      type = types.bool;
       description =
         "Whether to run a user-domain instance of orchestratord (default: true)";
       default = true;
     };
-    cloudAutoSync = lib.mkOption {
-      type = lib.types.bool;
+    cloudAutoSync = mkOption {
+      type = types.bool;
       description = "Whether to automatically sync cloud dirs (default: true)";
       default = true;
     };
-    cloudAutoSyncInterval = lib.mkOption {
-      type = lib.types.int;
+    cloudAutoSyncInterval = mkOption {
+      type = types.int;
       description = "Interval (in minutes) of cloud dirs sync (default: 10)";
       default = 10;
     };
-    cloudDirs = lib.mkOption { # ^^^^ TODO trace all dependents
-      type = lib.types.listOf lib.types.attrs;
+    cloudDirs = mkOption { # ^^^^ TODO trace all dependents
+      type = types.listOf types.attrs;
       description =
         "List of {name,cloudname,dirname} attributes defining the syncable directories by rcrsync";
       # default = [ # ^^^^ TODO make this build work
@@ -105,18 +106,18 @@ in {
         }
       ];
     };
-    editor = lib.mkOption {
-      type = lib.types.str;
+    editor = mkOption {
+      type = types.str;
       description = "Code editor (executable) of choice";
       default = "codium";
     };
-    browserExec = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
+    browserExec = mkOption {
+      type = types.nullOr types.str;
       description = "Executable name to open your browser of choice";
       default = null;
     };
-    screenResolution = lib.mkOption {
-      type = lib.types.str;
+    screenResolution = mkOption {
+      type = types.str;
       description = "Screen resolution in [width]x[height] format";
       default = "1920x1080";
     };
