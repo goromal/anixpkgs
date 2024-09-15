99abe946b5ad500891cabd4c83bb7b754494d619
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-09-14 23:34:33 -0700

    finish sitl module

--- pkgs/cxx-packages/arducopter/sitl-module.nix
+++ pkgs/cxx-packages/arducopter/sitl-module.nix
@@ -1,220 +1,90 @@
 { pkgs, config, lib, ... }:
-# ^^^^ TODO
-with lib;
-with lib.trivial;
 let
   cfg = config.services.ardupilot-sim;
-
-  # Define the path to the mutable configuration directory, within the rootDir or its subdirectory
-  runtimeConfigDir =
-    "${cfg.rootDir}/config"; # You can change 'config' to any subdirectory name
-
-  # Use the writeText function to create a file in the Nix store with the desired contents
-  startingLocationEnvFile = pkgs.writeText "starting-location.env" ''
-    LAT=${toString cfg.initialPose.lat}
-    LON=${toString cfg.initialPose.lon}
-    ALT=${toString cfg.initialPose.alt}
-    HDG=${toString cfg.initialPose.heading}
-  '';
-
-  simEnvSetupScript = pkgs.writeShellScriptBin "setup-sim-start-loc" ''
-    #!/bin/sh
-
-    exec > /tmp/prestart.log 2>&1
-    echo "Starting pre-start script"
-
-    set -euo pipefail
-
-    ENV_FILE="${runtimeConfigDir}/starting-location.env"
-
-    # Check if the environment file already exists
-    if [ ! -f "$ENV_FILE" ]; then
-      echo "should create ENV FILE"
-      # Write the environment variables to the starting-location.env file
-      echo "LAT=${toString cfg.initialPose.lat}" > "$ENV_FILE"
-      echo "LON=${toString cfg.initialPose.lon}" >> "$ENV_FILE"
-      echo "ALT=${toString cfg.initialPose.alt}" >> "$ENV_FILE"
-      echo "HDG=${toString cfg.initialPose.heading}" >> "$ENV_FILE"
-
-      # Ensure the file is owned by the 'andrew' user and group
-      chown andrew:andrew "$ENV_FILE"
-    fi
-    echo "Finished pre-start script"
-  '';
-
-  simStartScript = pkgs.writeShellScriptBin "ardupilot-sim-start" ''
-    #!/bin/sh
-    set -e
-
-    # Load environment variables from the environment file
-    source /data/andrew/ardupilot-sim/config/starting-location.env
-
-    # Debug: Print environment variables to a file
-    echo "LAT=$LAT, LON=$LON, ALT=$ALT, HDG=$HDG" > /tmp/ardupilot-debug-env
-
-    # Run the ardupilot executable with the environment variables
-    exec ${cfg.package}/bin/ardupilot \
-      --model=${cfg.model} \
-      --home="$LAT,$LON,$ALT,$HDG" \
-      --config=undulation:${toString cfg.undulation} \
-      -S -I ${toString cfg.asset-id} --serial5=sim:vicon: \
-      ${lib.optionalString (cfg.params != "") "--defaults ${cfg.params}"} \
-      ${lib.optionalString (cfg.speedUp != null) "--speedup ${cfg.speedUp}"} \
-      ${lib.optionalString cfg.wipe "--wipe"}
-  '';
-
-  # This port is specified from ardupilot. Do not change this value unless you know what you are doing.
-  defaultPort = 5760;
-  customTypes = {
-    ardupilotPort = mkOptionType {
-      name = "port";
-      description = "Ardupilot SITL port number";
-      check = x: isInt x && x >= defaultPort && mod x 10 == 0;
-      merge = mergeEqualOption;
-    };
-    latitude = mkOptionType {
-      name = "latitude";
-      description = "Geodetic latitude";
-      check = x: isFloat x && x >= -90.0 && x <= 90.0;
-      merge = mergeEqualOption;
-    };
-    longitude = mkOptionType {
-      name = "longitude";
-      description = "Geodetic longitude";
-      check = x: isFloat x && x >= -180.0 && x <= 180.0;
-      merge = mergeEqualOption;
-    };
-    heading = mkOptionType {
-      name = "heading";
-      description = "Heading";
-      check = x: isFloat x && x >= 0.0 && x <= 360.0;
-      merge = mergeEqualOption;
-    };
-    # This is used to specify which andrew defined ardupilot version to use.
-    type = types.enum (builtins.attrNames pkgs.andrew.ardupilot);
-    # The model type to simulate.
-    model =
-      types.enum [ "heli" "quad" "agentsim" "copter" "plane" "quadplane" ];
+  defaults = {
+    LAT = "34.381441";
+    LON = "-118.580861";
+    ALT = "417.0";
+    HDG = "0.0";
+    UND = "0.0";
   };
-
+  simEnvFileName = "sim.env";
+  simEnvFile = pkgs.writeText "${simEnvFileName}" ''
+    LAT=${defaults.LAT}
+    LON=${defaults.LON}
+    ALT=${defaults.ALT}
+    HDG=${defaults.HDG}
+    UND=${defaults.UND}
+  '';
+  simEnvSetupScript = pkgs.writeShellScriptBin "setup-sim-env" ''
+    ENV_FILE="${cfg.rootDir}/${simEnvFileName}"
+    if [ ! -f "$ENV_FILE" ]; then
+      echo "LAT=${defaults.LAT}" > "$ENV_FILE"
+      echo "LON=${defaults.LON}" >> "$ENV_FILE"
+      echo "ALT=${defaults.ALT}" >> "$ENV_FILE"
+      echo "HDG=${defaults.HDG}" >> "$ENV_FILE"
+      echo "UND=${defaults.UND}" >> "$ENV_FILE"
+      chown ${cfg.user}:${cfg.group} "$ENV_FILE"
+    fi
+  '';
+  simExecScript = pkgs.writeShellScriptBin "sim-exec-start" ''
+    source "${cfg.rootDir}/${simEnvFileName}"
+    exec ${cfg.package}/bin/ardupilot \
+      --model=quad \
+      --home="$LAT,$LON,$ALT,$HDG" \
+      --config=undulation:$UND \
+      -S -I 0 \
+      --defaults ${cfg.paramfile}
+  '';
 in {
   options.services.ardupilot-sim = with types; {
-    enable = mkEnableOption "Ardupilot heli Simulator";
-    rootDir = mkOption {
-      type = types.str;
-      description = "Ardupilot sim directory for data and configuration";
-      default = "/data/andrew/ardupilot-sim";
+    enable = mkEnableOption "Enable Arducopter SITL";
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "The ardupilot package to use";
     };
-    asset-id = mkOption {
-      type = types.int;
-      default = 2;
-      description = ''
-        Define the asset ID, which will be taken as the instance number.
-      '';
+    rootDir = lib.mkOption {
+      type = lib.types.str;
+      description = "Root service directory";
     };
-    initialPose = {
-      lat = mkOption {
-        type = customTypes.latitude;
-        description = "The initial latitude of the ardupilot simulator";
-        default = 33.467945;
-      };
-      lon = mkOption {
-        type = customTypes.longitude;
-        description = "The initial longitude of the ardupilot simulator";
-        default = -117.563854;
-      };
-      alt = mkOption {
-        type = types.float;
-        description = "The initial altitude of the ardupilot simulator";
-        default = 81.0;
-      };
-      heading = mkOption {
-        type = customTypes.heading;
-        description = "The initial heading of the ardupilot simulator";
-        default = 45.0;
-      };
+    user = lib.mkOption {
+      type = lib.types.str;
+      description = "Service owner user";
     };
-    undulation = mkOption {
-      type = types.int;
-      default = -34;
-      description = "The undulation HAE - MSL at initial location";
+    group = lib.mkOption {
+      type = lib.types.str;
+      description = "Service owner group";
     };
-    port = mkOption {
-      type = customTypes.ardupilotPort;
-      description = "The port to run the ardupilot SITL on";
-      default = defaultPort;
-    };
-    params = mkOption {
-      type = types.path;
-      description = "Absolute path to default mavlink parameters";
-    };
-    type = mkOption {
-      type = customTypes.type;
-      description = "Which andrew NixPkgs defined Ardupilot type to simulate";
-    };
-    model = mkOption {
-      type = customTypes.model;
-      description = "The model air frame type to simulate";
-    };
-    package = mkOption {
-      type = types.package;
-      description = "The ArduPilot package to run";
-      default = pkgs.andrew.ardupilot."${cfg.type}".sim;
-    };
-    logDir = mkOption {
-      type = types.nullOr types.str;
-      description = "Directory that symlinks to the simulated logs";
-      default = null;
-    };
-    speedUp = mkOption {
-      type = types.nullOr types.str;
-      description = "Speed up factor";
-      default = null;
-    };
-    wipe = mkOption {
-      type = types.bool;
-      description = "Wipe the EEPROM on startup";
-      default = false;
+    paramfile = lib.mkOption {
+      type = lib.types.str;
+      description = "Ardupilot param file";
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     systemd.tmpfiles.rules = [
-      "d ${cfg.rootDir}          - andrew andrew"
-      "d ${cfg.rootDir}/var      - andrew andrew"
-      "d ${runtimeConfigDir}     - andrew andrew"
-      "d ${cfg.rootDir}/var/data - andrew andrew"
-      "r! ${cfg.rootDir}/var/data/eeprom.bin - andrew andrew"
-      "r! ${cfg.rootDir}/var/data/persistent.dat - andrew andrew"
-      "R ${cfg.rootDir}/var/data/terrain - andrew andrew"
-      "R ${cfg.rootDir}/var/data/scripts - andrew andrew"
-      "d ${cfg.rootDir}/var/data/logs - andrew andrew"
-      "Z ${cfg.rootDir}          - andrew andrew"
-      "L ${cfg.rootDir}/var/data/scripts - - - - ${cfg.package}/scripts"
-      "C ${runtimeConfigDir}/starting-location.env 0666 andrew andrew ${startingLocationEnvFile}"
-    ] ++ (if cfg.logDir == null then
-      [ ]
-    else [
-      "d ${cfg.logDir}          - andrew andrew"
-      "L+ ${cfg.logDir} - - - -  ${cfg.rootDir}/var/data/logs"
-      "f ${cfg.logDir}/* - - - 0666"
-    ]);
-
-    systemd.services.ardupilot-sim = {
+      "d ${cfg.rootDir} - ${cfg.user} ${cfg.group}"
+      "Z ${cfg.rootDir} - ${cfg.user} ${cfg.group}"
+      "C ${cfg.rootDir}/${simEnvFileName} 0666 ${cfg.user} ${cfg.group} ${simEnvFile}"
+      "d ${cfg.rootDir}/logs - ${cfg.user} ${cfg.group}"
+      "f ${cfg.rootDir}/logs/* - - - 0666"
+      "r! ${cfg.rootDir}/eeprom.bin - ${cfg.user} ${cfg.group}"
+      "r! ${cfg.rootDir}/persistent.dat - ${cfg.user} ${cfg.group}"
+      "R ${cfg.rootDir}/terrain - ${cfg.user} ${cfg.group}"
+      "R ${cfg.rootDir}/scripts - ${cfg.user} ${cfg.group}"
+    ];
+    systemd.services.ardusitl = {
       enable = true;
-      description = "Simulator for the Ardupilot Heli flight controller";
+      description = "Arducopter SITL";
       serviceConfig = {
         Type = "simple";
-        ExecStartPre = "${simEnvSetupScript}/bin/setup-sim-start-loc";
-        ExecStart = "${simStartScript}/bin/ardupilot-sim-start";
-
-        WorkingDirectory = "${cfg.rootDir}/var/data";
+        ExecStartPre = "${simEnvSetupScript}/bin/setup-sim-env";
+        ExecStart = "${simExecScript}/bin/sim-exec-start";
+        WorkingDirectory = "${cfg.rootDir}";
+        ReadWritePaths = [ "${cfg.rootDir}" ];
+        User = cfg.user;
+        Group = cfg.group;
         PrivateTmp = true;
-        ReadWritePaths = [ "${cfg.rootDir}/var/data" ];
-
-        User = "andrew";
-        Group = "andrew";
-
         AmbientCapabilities = [ "CAP_KILL" ];
         CapabilityBoundingSet = [ "CAP_KILL" ];
       };
