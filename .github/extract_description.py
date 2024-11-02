7fd150719c7e29429eca1c12b491ff613d01de52
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-11-01 21:54:46 -0700

    try again

--- .github/extract_description.py
+++ .github/extract_description.py
@@ -6,14 +6,16 @@ import sys
 comment_body = sys.argv[1]
 
 # Regex to find the change description and the checkbox
-description_match = re.search(r"## Change Description\s+\n+(.+?)\n", comment_body, re.DOTALL)
+description_match = re.search(r"## Change Description\n+\s+(.+?)\n", comment_body, re.DOTALL)
 checkbox_marked = re.search(r"- \[x\] Add to global changelog", comment_body, re.IGNORECASE)
 
 if description_match and checkbox_marked:
     # Extract and clean the change description text
     change_description = description_match.group(1).strip()
     
-    # Set the change description as an output for GitHub Actions
-    print(f"::set-output name=CHANGE_DESCRIPTION::{change_description}")
+    if change_description:
+        print(f"::set-output name=CHANGE_DESCRIPTION::{change_description}")
+    else:
+        print(f"::set-output name=CHANGE_DESCRIPTION::NONE")
 else:
     print(f"::set-output name=CHANGE_DESCRIPTION::NONE")
