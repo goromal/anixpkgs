79ef4f78960e001c314103e947ddee405c3801c9
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-11-06 22:12:01 -0800

    merge

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
     geometry.url = "github:goromal/geometry";
     geometry.flake = false;
 
-    gmail-parser.url = "github:goromal/gmail_parser?ref=dev/lint-and-fix";
+    gmail-parser.url = "github:goromal/gmail_parser";
     gmail-parser.flake = false;
 
     makepyshell.url =
