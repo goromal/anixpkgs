06c2d978eb96c2409b4d8195e1ba1104222a15ca
goromal (bot) <goromal.bot@gmail.com>
2024-11-25 03:49:13 +0000

    Lint format

--- pkgs/bash-packages/rcdo/default.nix
+++ pkgs/bash-packages/rcdo/default.nix
@@ -1,36 +1,34 @@
-{ callPackage
-, writeShellScriptBin
-}:
+{ callPackage, writeShellScriptBin }:
 let
-    pkgname = "rcdo";
-    usagestr = ''
+  pkgname = "rcdo";
+  usagestr = ''
     ${pkgname} [command]
-    
+
     Run local [command] on a remote machine via ssh/scp.
-    
+
     Examples:
-    
+
     1) One-liner for running remote commands on a remote machine:
-    
+
       ${pkgname} at:user@hostname ls ~/remote/dir
-    
+
     2) Run a local conversion tool on a remote video file ("remote input"), storing the result locally:
-    
+
       ${pkgname} mp4 in:user@hostname:~/video.mov ~/converted_vid.mp4
-    
+
     3) Run a local conversion tool on a local video file and store the result remotely ("remote output"):
-    
+
       ${pkgname} mp4 ~/video.mov out:user@hostname:~/converted_vid.mp4
-    '';
+  '';
 in (writeShellScriptBin pkgname ''
-    echo "TODO"
+  echo "TODO"
 '') // {
-    meta = {
-        description = "Run local commands on remote files.";
-        longDescription = '' 
-            ```bash
-            ${usagestr}
-            ``` 
-        '';
-    };
+  meta = {
+    description = "Run local commands on remote files.";
+    longDescription = ''
+      ```bash
+      ${usagestr}
+      ``` 
+    '';
+  };
 }
