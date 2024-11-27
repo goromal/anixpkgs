ad882f3d4517d6d31d71007c27990988ab24bab5
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-11-27 09:42:18 -0800

    Update default.nix

--- pkgs/bash-packages/cpp-helper/default.nix
+++ pkgs/bash-packages/cpp-helper/default.nix
@@ -6,15 +6,15 @@ let
     usage: ${pkgname} [options]
 
     Options:
+        exec-lib   CPPNAME      Generate a lib+exec package template
+        header-lib CPPNAME      Generate a header-only library template
+        format-file             Dumps a format rules file into .clang-format
+
+        nix                     Dump template shell.nix file
+        vscode                  Generate VSCode C++ header detection settings file
         make       TARGET|all   Full CMake build command (run from repo root)
         challenge  TARGET|all   Full CMake build command WITH SANITIZERS
                                 (run from repo root)
-        format-file             Dumps a format rules file into .clang-format
-        nix                     Dump template shell.nix file
-        exec-lib   CPPNAME      Generate a lib+exec package template
-        header-lib CPPNAME      Generate a header-only library template
-        vscode                  Generate VSCode C++ header detection settings file
-                                (Run inside a Nix dev environment)
   '';
   printErr = "${color-prints}/bin/echo_red";
   printWrn = "${color-prints}/bin/echo_yellow";
