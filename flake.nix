ddfbe7bdbb92dc2fa91c4758a3da1b473a5b9f35
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-03-10 22:40:14 -0700

    merge orchestrator

--- flake.nix
+++ flake.nix
@@ -99,7 +99,7 @@
     mesh-plotter.url = "github:goromal/mesh-plotter";
     mesh-plotter.flake = false;
 
-    orchestrator.url = "github:goromal/orchestrator?ref=dev/metrics";
+    orchestrator.url = "github:goromal/orchestrator";
     orchestrator.flake = false;
 
     photos-tools.url = "github:goromal/photos-tools";
