f9f892b82ccb4d5c66d0b1d969e11b100a908914
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-04-11 23:10:46 -0700

    Machine webservers (#354)
    Co-authored-by: goromal (bot) <goromal.bot@gmail.com>

--- pkgs/modules/ats/modules.nix
+++ pkgs/modules/ats/modules.nix
@@ -4,7 +4,7 @@ let
   globalCfg = config.machines.base;
   cfg = config.services.ats;
   wiki-url = if globalCfg.serveNotesWiki then
-    "http://localhost:${builtins.toString globalCfg.notesWikiPort}"
+    "http://${config.networking.hostName}.local"
   else
     "https://notes.andrewtorgesen.com";
   atsudo = pkgs.writeShellScriptBin "atsudo" ''
