21002b2b4f79c2807ef7618fc0be0da0ec8cdcb2
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-04-20 18:32:07 -0700

    ready for primetime

--- pkgs/bash-packages/la-quiz/quiz.py
+++ pkgs/bash-packages/la-quiz/quiz.py
@@ -5,7 +5,6 @@ import sys
 from tkinter import *
 from PIL import Image, ImageTk
 
-# ^^^^ TODO -1 indicates we need to append a (HELP ME) to the title; write the click coordinates to file
 
 def main():
     root = Tk()
@@ -33,55 +32,75 @@ def main():
     class QuizDriver:
         AWAITING_INPUT = 0
         GRADING_INPUT = 1
+
         def __init__(self, answers_json, debug_mode):
             self.keyfile = answers_json
+            self.num_cities = 0
             with open(self.keyfile, "r") as answer_file:
-                self.cities = json.loads(answer_file.read())["cities"]
+                self.num_cities = len(json.loads(answer_file.read())["cities"])
+            self.city_indices = list(range(self.num_cities))
             self.city_idx = 0
-            random.shuffle(self.cities)
+            random.shuffle(self.city_indices)
             self.state = QuizDriver.AWAITING_INPUT
             self.debug_mode = debug_mode
+
         def handle_click(self, event):
-            fig_title = f"{self.cities[self.city_idx]['name']}{' (HELP ME)' if self.debug_mode else ''}"
+            with open(self.keyfile, "r") as answer_file:
+                cities_json = json.loads(answer_file.read())
+
+            x = cities_json["cities"][self.city_indices[self.city_idx]]["x"]
+            y = cities_json["cities"][self.city_indices[self.city_idx]]["y"]
+
+            fig_title = f"{cities_json['cities'][self.city_indices[self.city_idx]]['name']}{' (HELP ME)' if self.debug_mode and (x < 0 or y < 0) else ''}"
             root.title(fig_title)
             canvas.delete("marker")
             xc, yc = event.x, event.y
+
             if self.state == QuizDriver.AWAITING_INPUT:
                 self.state = QuizDriver.GRADING_INPUT
             else:
-                x = self.cities[self.city_idx]["x"]
-                y = self.cities[self.city_idx]["y"]
                 if self.debug_mode:
-                    canvas.create_text(xc, yc, text=f"({xc}, {yc})", fill="red", tags="marker")
+                    canvas.create_text(
+                        xc, yc, text=f"({xc}, {yc})", fill="red", tags="marker"
+                    )
                     if x == -1 or y == -1:
                         x = xc
                         y = yc
-                        self.cities[self.city_idx]["x"] = xc
-                        self.cities[self.city_idx]["y"] = yc
-                        with open(self.keyfile, "r") as infile:
-                            orig_json = json.loads(infile.read())
-                        # ^^^^ TODO this is out of order
-                        orig_json["cities"][self.city_idx]["x"] = xc
-                        orig_json["cities"][self.city_idx]["y"] = yc
-                        with open(self.keyfile, "w") as outfile:
-                            outfile.write(json.dumps(orig_json))
+                        cities_json["cities"][self.city_indices[self.city_idx]][
+                            "x"
+                        ] = xc
+                        cities_json["cities"][self.city_indices[self.city_idx]][
+                            "y"
+                        ] = yc
+                        with open(self.keyfile, "w") as answer_file:
+                            answer_file.write(json.dumps(cities_json))
 
                 if x >= 0 and y >= 0:
-                    if math.sqrt(float((x-xc)*(x-xc)) + float((y-yc)*(y-yc))) < 20:
-                        canvas.create_oval(x-5, y-5, x+5, y+5, fill="green", tags="marker")
+                    if (
+                        math.sqrt(
+                            float((x - xc) * (x - xc)) + float((y - yc) * (y - yc))
+                        )
+                        < 20
+                    ):
+                        canvas.create_oval(
+                            x - 5, y - 5, x + 5, y + 5, fill="green", tags="marker"
+                        )
                     else:
-                        canvas.create_oval(x-5, y-5, x+5, y+5, fill="red", tags="marker")
+                        canvas.create_oval(
+                            x - 5, y - 5, x + 5, y + 5, fill="red", tags="marker"
+                        )
                 self.state = QuizDriver.AWAITING_INPUT
-                if self.city_idx < len(self.cities) - 1:
+                if self.city_idx < self.num_cities - 1:
                     self.city_idx += 1
                 else:
                     self.city_idx = 0
-                    random.shuffle(self.cities)
-    
+                    random.shuffle(self.city_indices)
+
     quiz = QuizDriver(ansfile, debug_mode)
     canvas.bind("<Button-1>", quiz.handle_click)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
