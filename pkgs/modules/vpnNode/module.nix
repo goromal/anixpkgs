7f3b6cef9bacd8b0e7a32eb65b86022ca42cfb39
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-06-13 14:36:17 -0700

    fix interface name

--- pkgs/modules/vpnNode/module.nix
+++ pkgs/modules/vpnNode/module.nix
@@ -50,11 +50,11 @@ in {
           # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
           # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
           postSetup = ''
-            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
           '';
           # This undoes the above command
           postShutdown = ''
-            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
           '';
           privateKeyFile = cfg.privateKeyPath;
           peers = [{
