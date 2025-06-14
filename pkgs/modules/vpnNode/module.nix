406e4ea6e8480b9b6dba3f8f6e04a722fd86515f
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-06-13 22:40:13 -0700

    wipp

--- pkgs/modules/vpnNode/module.nix
+++ pkgs/modules/vpnNode/module.nix
@@ -50,15 +50,15 @@ in {
           ips = [ "10.100.0.1/24" ];
           # The port that WireGuard listens to. Must be accessible by the client.
           listenPort = service-ports.wireguard;
-          # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-          # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-          postSetup = ''
-            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
-          '';
-          # This undoes the above command
-          postShutdown = ''
-            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
-          '';
+          # # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+          # # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+          # postSetup = ''
+          #   ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
+          # '';
+          # # This undoes the above command
+          # postShutdown = ''
+          #   ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ${globalCfg.wanInterface} -j MASQUERADE
+          # '';
           privateKeyFile = cfg.privateKeyPath;
           peers = [{
             publicKey = cfg.publicKey;
