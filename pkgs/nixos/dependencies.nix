a8f42e093cebe8741edea2b332a1309976209cde
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-06-15 14:47:24 -0700

    impl

--- pkgs/nixos/dependencies.nix
+++ pkgs/nixos/dependencies.nix
@@ -31,7 +31,7 @@ in rec {
             serviceConfig = {
               Type = "oneshot";
               ExecStart =
-                "${anixpkgs.orchestrator}/bin/orchestrator bash 'bash ${jobShellScript}'"; # ^^^^ TODO can this be referenced?
+                "${anixpkgs.orchestrator}/bin/orchestrator bash 'bash ${jobShellScript}'";
               ReadWritePaths = readWritePaths;
             };
           };
--- pkgs/nixos/profiles/ats.nix
+++ pkgs/nixos/profiles/ats.nix
@@ -138,7 +138,7 @@ with import ../dependencies.nix; {
         };
       }
       {
-        name = "ats-grader"; # ^^^^ TODO how to trigger this from another job?
+        name = "ats-grader";
         jobShellScript = pkgs.writeShellScript "ats-grader" ''
           authm refresh --headless || { logger -t ats-grader "Authm refresh UNSUCCESSFUL"; >&2 echo "authm refresh error!"; exit 1; }
           tmpdir=$(mktemp -d)
@@ -226,7 +226,6 @@ with import ../dependencies.nix; {
       anixpkgs.scrape
       anixpkgs.providence-tasker
       anixpkgs.daily_tactical_server
-      pkgs.systemd # ^^^^ TODO useful without atsudo?
     ];
   }) // {
     users.users.andrew.hashedPassword = lib.mkForce
