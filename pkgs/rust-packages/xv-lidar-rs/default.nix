eb74f74c71c6426d4b908ff6ab80d974773abbd1
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-07-08 19:38:55 -0700

    rust

--- pkgs/rust-packages/xv-lidar-rs/default.nix
+++ pkgs/rust-packages/xv-lidar-rs/default.nix
@@ -3,6 +3,7 @@ rustPlatform.buildRustPackage rec {
   pname = "xv-lidar-rs";
   version = "0.0.1";
   src = pkg-src;
+  useFetchCargoVendor = true;
   cargoHash =
     "sha256-AO01Q2gyrwtjy2ng5TxkT3qh1C/WBIRjSR7USeObpwg="; # lib.fakeHash
   meta = {
