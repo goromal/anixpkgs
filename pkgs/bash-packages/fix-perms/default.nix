d6ca80343d5c5d8b836799663e188ac762032984
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-07-15 20:35:52 -0700

    Augment docs for fix-perms

--- pkgs/bash-packages/fix-perms/default.nix
+++ pkgs/bash-packages/fix-perms/default.nix
@@ -8,6 +8,19 @@ in (writeArgparseScriptBin pkgname ''
   usage: ${pkgname} dir
 
   Recursively claim ownership of all files and folders in dir. Attempts to deduce special cases such as ~/.ssh/*.
+
+  EXAMPLES:
+
+  Current directory is ~/.ssh:
+
+    find . -type d -exec chmod 700 {} \;
+    find . -type f -exec chmod 600 {} \;
+    find . -type f -name \*.pub -exec chmod 644 {} \;
+
+  Current directory is a normal directory:
+
+    find . -type d -exec chmod 755 {} \;
+    find . -type f -exec chmod 644 {} \;
 '' [ ] ''
   if [[ -z "$1" ]]; then
       ${printErr} "No dir provided."
