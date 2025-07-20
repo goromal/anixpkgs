ee7a8bc096fb74f3d63496f5e78177c893c5761a
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-07-19 23:45:11 -0700

    more

--- pkgs/bash-packages/bash-utils/genusagedoc.nix
+++ pkgs/bash-packages/bash-utils/genusagedoc.nix
@@ -4,14 +4,14 @@ let
     echo -e "\n\n### ${x}" >> $out/helpstr.txt
     echo -e "\n\n\`\`\`bash" >> $out/helpstr.txt
     "$xc" ${x} ${helpCmd} >> $out/helpstr.txt
-    echo "\`\`\`" >> $out/helpstr.txt
+    echo -n "\`\`\`" >> $out/helpstr.txt
   '') subCmds);
   cmdOutputFile = ((runCommand "mh" { } ''
     mkdir $out
     for xc in "${packageAttr}/bin"/*; do
       echo "\`\`\`bash" >> $out/helpstr.txt
       "$xc" ${helpCmd} >> $out/helpstr.txt
-      echo "\`\`\`" >> $out/helpstr.txt
+      echo -n "\`\`\`" >> $out/helpstr.txt
       ${mkSubCmds}
     done  
   '') + "/helpstr.txt");
