ed856dbe235ee48be7a913e229cf3a195470f734
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-09-14 22:33:54 -0700

    lets test

--- pkgs/modules/plexNode/module.nix
+++ pkgs/modules/plexNode/module.nix
@@ -7,10 +7,30 @@ in {
     enable = lib.mkEnableOption "enable plex node services";
   };
   config = lib.mkIf cfg.enable {
+    # Hard drive partition mount
     fileSystems."/mnt/media-empire" = {
       device = "/dev/disk/by-uuid/40E4C87FE4C878A4";
       fsType = "ntfs-3g";
       options = [ "defaults" "nofail" "uid=1000" "gid=100" "umask=022" ];
     };
+
+    # Enable Plex Media Server
+    services.plex = {
+      enable = true;
+      openFirewall = true;
+      # Default Plex data directory
+      dataDir = "/var/lib/plex";
+    };
+
+    # Define a 'media' group
+    users.groups.media = {};
+
+    # Make sure the Plex user can read media
+    users.users.plex.extraGroups = [ "media" ];
+
+    # Ensure the drive has appropriate permissions
+    systemd.tmpfiles.rules = [
+      "d /mnt/media-empire 0755 plex media -"
+    ];
   };
 }
