d456a75e91b408512f92c22c22376b0754dca0f6
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-09-21 19:34:32 -0700

    wip

--- pkgs/modules/matrixNode/module.nix
+++ pkgs/modules/matrixNode/module.nix
@@ -29,6 +29,15 @@ in {
         name = "matrix-synapse";
         ensureDBOwnership = true;
       }];
+      initialScript = pkgs.writeText "synapse-init.sql" ''
+        DROP DATABASE IF EXISTS "matrix-synapse";
+        CREATE DATABASE "matrix-synapse"
+          WITH OWNER "matrix-synapse"
+          TEMPLATE template0
+          LC_COLLATE = 'C'
+          LC_CTYPE = 'C'
+          ENCODING = 'UTF8';
+      '';
     };
 
     services.matrix-synapse = {
