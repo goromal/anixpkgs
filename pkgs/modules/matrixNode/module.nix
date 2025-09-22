0b9cc4f5bbdfa3f44ecb91fbb23f19ce4d0fd6a6
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-09-21 19:25:19 -0700

    wip

--- pkgs/modules/matrixNode/module.nix
+++ pkgs/modules/matrixNode/module.nix
@@ -1,6 +1,8 @@
 { pkgs, config, lib, ... }:
 with import ../../nixos/dependencies.nix;
-let cfg = config.services.matrixNode;
+let
+  globalCfg = config.machines.base;
+  cfg = config.services.matrixNode;
 in {
   options.services.matrixNode = {
     enable = lib.mkEnableOption "enable matrix node services";
@@ -12,20 +14,33 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
+    # Before deploying synapse server, a postgresql database must be set up. For that, please make sure that postgresql is running and the following SQL statements to create a user & database called matrix-synapse were executed before synapse starts up:
+    #
+    # CREATE ROLE "matrix-synapse";
+    # CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+    #   TEMPLATE template0
+    #   LC_COLLATE = "C"
+    #   LC_CTYPE = "C";
+    services.postgresql = {
+      enable = true;
+      # package = pkgs.postgresql_15; # or pkgs.postgresql_16 if you want the newest
+      ensureDatabases = [ "matrix-synapse" ];
+      ensureUsers = [{
+        name = "matrix-synapse";
+        ensureDBOwnership = true;
+      }];
+    };
+
     services.matrix-synapse = {
       enable = true;
-      settings = {
-        server_name = "ats";
-        listeners = [{
-          port = service-ports.matrix;
-          bind_addresses = [ "0.0.0.0" "::" ];
-          type = "http";
-          tls = false; # Only for LAN
-          resources = [{
-            names = [ "client" "federation" ];
-            compress = false;
-          }];
-        }];
+      settings.server_name = config.networking.hostName;
+      settings.public_baseurl = "http://${config.networking.hostName}.local:8008/";
+      database_type = "psycopg2";
+      database_config = {
+        name = "matrix-synapse";
+        user = "matrix-synapse";
+        password = ""; # if you want password auth, set it via a secret
+        host = "/run/postgresql"; # unix socket path
       };
     };
 
