3bfa1752fae5b4c837c2b2faa8c1b2702b63ec62
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-05 22:46:50 -0800

    fix

--- pkgs/modules/mailNode/module.nix
+++ pkgs/modules/mailNode/module.nix
@@ -33,8 +33,9 @@ in {
     };
     users.groups.goromail = { gid = 993; };
 
-    systemd.tmpfiles.rules =
-      [ "d ${globalCfg.homeDir}/mail 0750 andrew dev -" ];
+    systemd.tmpfiles.rules = [
+      "d ${globalCfg.homeDir}/mail 0770 andrew dev -"
+    ];
     users.users.goromail.extraGroups = [ "dev" ];
     fileSystems."/var/mail/goromail" = {
       device = "${globalCfg.homeDir}/mail";
