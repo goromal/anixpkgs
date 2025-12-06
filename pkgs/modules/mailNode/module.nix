e978b3b6905e83eca53cbac81d4e5af9c69a3523
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-05 20:54:01 -0800

    wip test

--- pkgs/modules/mailNode/module.nix
+++ pkgs/modules/mailNode/module.nix
@@ -8,19 +8,17 @@ in {
     # Postfix: receive-only mail server
     services.postfix = {
       enable = true;
-
-      # Modern configuration location (NixOS ≥ 23.11)
-      settings = {
-        myhostname = "mail.andrewtorgesen.com";
-        mydomain = "andrewtorgesen.com";
-
-        # Domains Postfix should treat as local email
-        # (i.e., mail delivered locally, not relayed)
-        mydestination = "localhost, $myhostname, $mydomain";
-
-        # Local delivery to /var/mail/<user>
-        home_mailbox = "";
-        mailbox_command = ""; # default local delivery agent
+      hostname = "mail.andrewtorgesen.com";
+      domain = "andrewtorgesen.com";
+      # mapFiles = {
+      #   virtual_mailbox_maps = ./virtual_mailbox_maps;
+      # };
+      config = {
+        virtual_mailbox_base = "/var/mail/goromail/";
+        # virtual_mailbox_maps = "hash:/etc/postfix/virtual_mailbox_maps";
+        virtual_mailbox_domains = "andrewtorgesen.com";
+        virtual_uid_maps = "static:994";
+        virtual_gid_maps = "static:993";
       };
     };
 
@@ -32,32 +30,19 @@ in {
       isSystemUser = true;
       home = "/var/mail/goromail";
       createHome = false; # mailboxes get created automatically
+      uid = 994;
+      group = "goromail";
+    };
+    users.groups.goromail = {
+      gid = 993;
     };
 
-    # OPTIONAL: pipe incoming mail automatically to a script
-    # (Uncomment this block if you want your program to process messages immediately)
-    #
-    # services.postfix.config = ''
-    #   # Transport map: send all mail for goromail@yourdomain.com to a script
-    #   transport_maps = hash:/etc/postfix/transport;
-    # '';
-    #
-    # environment.etc."postfix/transport".text = ''
-    #   goromail@yourdomain.com  localrunner:
-    # '';
-    #
-    # services.postfix.extraConfig = ''
-    #   localrunner_destination_recipient_limit = 1
-    #   localrunner_destination_concurrency_limit = 1
-    #   mailbox_command = /usr/local/bin/process-email.sh
-    # '';
-
-    # Make sure Postfix can write mailboxes
-    security.wrappers.postdrop = {
-      owner = "root";
-      group = "postdrop";
-      capabilities = "cap_chown,cap_setgid,cap_setuid=ep";
-      source = "${pkgs.postfix}/libexec/postfix/postdrop";
-    };
+    # # Make sure Postfix can write mailboxes
+    # security.wrappers.postdrop = {
+    #   owner = "root";
+    #   group = "postdrop";
+    #   capabilities = "cap_chown,cap_setgid,cap_setuid=ep";
+    #   source = "${pkgs.postfix}/libexec/postfix/postdrop";
+    # };
   };
 }
