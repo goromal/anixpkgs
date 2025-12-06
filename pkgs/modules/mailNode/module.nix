104f0e576df7db0120860564b343077a72602f0f
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-05 22:21:57 -0800

    tmp user change

--- pkgs/modules/mailNode/module.nix
+++ pkgs/modules/mailNode/module.nix
@@ -9,42 +9,26 @@ in {
       enable = true;
       hostname = "mail.andrewtorgesen.com";
       domain = "andrewtorgesen.com";
-      mapFiles = {
-        # virtual_mailbox_maps = ./virtual_mailbox_maps;
-        transport = ./virtual_transport;
-      };
       config = {
         virtual_mailbox_base = "/var/mail/goromail/";
-        # virtual_mailbox_maps = "hash:/etc/postfix/virtual_mailbox_maps";
-        virtual_transport_maps = "hash:/etc/postfix/transport";
         virtual_mailbox_domains = "andrewtorgesen.com";
-        virtual_uid_maps = "static:994";
-        virtual_gid_maps = "static:993";
+        virtual_uid_maps = "static:1000"; # ^^^^ TODO: make configurable
+        virtual_gid_maps = "static:1000";
       };
-      extraMasterConf = ''
-        gorogatherer  unix  -  n  n  -  -  pipe
-          flags=Rq user=goromail argv=/usr/local/bin/gorogather -- ''${recipient}
-      '';
     };
-
-    environment.systemPackages = [
-      (pkgs.writeShellScriptBin "gorogather" ''
-        echo - > /data/andrew/TMPMAIL
-      '')
-    ];
 
     # Open the firewall
     # NOTE: requires router port-forwarding as well
     #       test with `nc -zv <public_ip> 25`
     networking.firewall.allowedTCPPorts = [ 25 ];
 
-    users.users.goromail = {
-      isSystemUser = true;
-      home = "/var/mail/goromail";
-      createHome = false;
-      uid = 994;
-      group = "goromail";
-    };
-    users.groups.goromail = { gid = 993; };
+    # users.users.goromail = {
+    #   isSystemUser = true;
+    #   home = "/var/mail/goromail";
+    #   createHome = false;
+    #   uid = 994;
+    #   group = "goromail";
+    # };
+    # users.groups.goromail = { gid = 993; };
   };
 }
