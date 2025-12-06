deff622af15e6c3bce0ae3aaccdcd6b3601a27bc
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-05 21:44:27 -0800

    wip

--- pkgs/modules/mailNode/module.nix
+++ pkgs/modules/mailNode/module.nix
@@ -5,17 +5,18 @@ in {
     enable = lib.mkEnableOption "enable mail node services";
   };
   config = lib.mkIf cfg.enable {
-    # Postfix: receive-only mail server
     services.postfix = {
       enable = true;
       hostname = "mail.andrewtorgesen.com";
       domain = "andrewtorgesen.com";
-      # mapFiles = {
-      #   virtual_mailbox_maps = ./virtual_mailbox_maps;
-      # };
+      mapFiles = {
+        # virtual_mailbox_maps = ./virtual_mailbox_maps;
+        transport = ./virtual_transport;
+      };
       config = {
         virtual_mailbox_base = "/var/mail/goromail/";
         # virtual_mailbox_maps = "hash:/etc/postfix/virtual_mailbox_maps";
+        virtual_transport_maps = "hash:/etc/postfix/transport";
         virtual_mailbox_domains = "andrewtorgesen.com";
         virtual_uid_maps = "static:994";
         virtual_gid_maps = "static:993";
@@ -23,24 +24,19 @@ in {
     };
 
     # Open the firewall
+    # NOTE: requires router port-forwarding as well
+    #       test with `nc -zv <public_ip> 25`
     networking.firewall.allowedTCPPorts = [ 25 ];
 
-    # Create a user that will receive mail
     users.users.goromail = {
       isSystemUser = true;
       home = "/var/mail/goromail";
-      createHome = false; # mailboxes get created automatically
+      createHome = false;
       uid = 994;
       group = "goromail";
     };
-    users.groups.goromail = { gid = 993; };
-
-    # # Make sure Postfix can write mailboxes
-    # security.wrappers.postdrop = {
-    #   owner = "root";
-    #   group = "postdrop";
-    #   capabilities = "cap_chown,cap_setgid,cap_setuid=ep";
-    #   source = "${pkgs.postfix}/libexec/postfix/postdrop";
-    # };
+    users.groups.goromail = {
+      gid = 993;
+    };
   };
 }
