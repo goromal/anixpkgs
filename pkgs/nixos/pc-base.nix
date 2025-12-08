0a2439306ee76b992132d00a81527608053ca700
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-07 23:02:57 -0800

    wip

--- pkgs/nixos/pc-base.nix
+++ pkgs/nixos/pc-base.nix
@@ -12,9 +12,9 @@ let
     args=''${args% }
     pw=$(${anixpkgs.sread}/bin/sread ${cfg.homeDir}/secrets/${config.networking.hostName}/p.txt.tyz)
     if [[ ! -z "$pw" ]]; then
-      echo "$pw" | sudo -S $args
+      echo "$pw" | ${pkgs.sudo}/bin/sudo -S $args
     else
-      sudo $args
+      ${pkgs.sudo}/bin/sudo $args
     fi
   '';
   atsudo-headless = pkgs.writeShellScriptBin "atsudo-headless" ''
@@ -25,7 +25,7 @@ let
     args=''${args% }
     pw=$(${anixpkgs.sread}/bin/sread ${cfg.homeDir}/secrets/${config.networking.hostName}/p.txt.tyz)
     if [[ ! -z "$pw" ]]; then
-      echo "$pw" | sudo -S $args
+      echo "$pw" | ${pkgs.sudo}/bin/sudo -S $args
     else
       exit 1
     fi
