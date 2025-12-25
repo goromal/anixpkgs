8bf972193414a77f0ed6d35c01a7eb1d1dc4fece
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-24 18:35:09 -0800

    [signals-cpp] Expose zeroized objects from signal class

--- flake.nix
+++ flake.nix
@@ -121,7 +121,7 @@
     secure-delete.url = "github:goromal/secure-delete";
     secure-delete.flake = false;
 
-    signals-cpp.url = "github:goromal/signals-cpp";
+    signals-cpp.url = "github:goromal/signals-cpp?ref=dev/zerotypes";
     signals-cpp.flake = false;
 
     simple-image-editor.url = "github:goromal/simple-image-editor";
