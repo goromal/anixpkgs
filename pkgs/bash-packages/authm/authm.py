4a867fe0c9b35d50b8ccbe1703306d99a6c25beb
Andrew Torgesen <andrew.torgesen@gmail.com>
2025-12-27 11:25:23 -0800

    [ATS] Deprecate bot and journal auth refreshes (#435)
    Co-authored-by: goromal (bot) <goromal.bot@gmail.com>

--- pkgs/bash-packages/authm/authm.py
+++ pkgs/bash-packages/authm/authm.py
@@ -40,36 +40,36 @@ def refresh(headless, force):
             f"Refresh of {GPD.getKwargsOrDefault('gmail_refresh_file')} needed."
         )
         exit(1)
-    print(Fore.YELLOW + "Refreshing Bot Tokens..." + Style.RESET_ALL)
-    try:
-        getGoogleService(
-            "gmail",
-            "v1",
-            GPD.getKwargsOrDefault("gmail_secrets_json"),
-            GPD.getKwargsOrDefault("gbot_refresh_file"),
-            headless=headless,
-            force=force,
-        )
-    except CredentialsRefreshException:
-        sys.stderr.write(
-            f"Refresh of {GPD.getKwargsOrDefault('gbot_refresh_file')} needed."
-        )
-        exit(1)
-    print(Fore.YELLOW + "Refreshing Journal Tokens..." + Style.RESET_ALL)
-    try:
-        getGoogleService(
-            "gmail",
-            "v1",
-            GPD.getKwargsOrDefault("gmail_secrets_json"),
-            GPD.getKwargsOrDefault("journal_refresh_file"),
-            headless=headless,
-            force=force,
-        )
-    except CredentialsRefreshException:
-        sys.stderr.write(
-            f"Refresh of {GPD.getKwargsOrDefault('journal_refresh_file')} needed."
-        )
-        exit(1)
+    # print(Fore.YELLOW + "Refreshing Bot Tokens..." + Style.RESET_ALL)
+    # try:
+    #     getGoogleService(
+    #         "gmail",
+    #         "v1",
+    #         GPD.getKwargsOrDefault("gmail_secrets_json"),
+    #         GPD.getKwargsOrDefault("gbot_refresh_file"),
+    #         headless=headless,
+    #         force=force,
+    #     )
+    # except CredentialsRefreshException:
+    #     sys.stderr.write(
+    #         f"Refresh of {GPD.getKwargsOrDefault('gbot_refresh_file')} needed."
+    #     )
+    #     exit(1)
+    # print(Fore.YELLOW + "Refreshing Journal Tokens..." + Style.RESET_ALL)
+    # try:
+    #     getGoogleService(
+    #         "gmail",
+    #         "v1",
+    #         GPD.getKwargsOrDefault("gmail_secrets_json"),
+    #         GPD.getKwargsOrDefault("journal_refresh_file"),
+    #         headless=headless,
+    #         force=force,
+    #     )
+    # except CredentialsRefreshException:
+    #     sys.stderr.write(
+    #         f"Refresh of {GPD.getKwargsOrDefault('journal_refresh_file')} needed."
+    #     )
+    #     exit(1)
     print(Fore.GREEN + "DONE" + Style.RESET_ALL)
 
 
