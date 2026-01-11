d525184af5a2b6e54325979068a7e70b29e5a1c6
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-01-10 22:55:11 -0800

    Update personal-inspiron.nix

--- pkgs/nixos/configurations/personal-inspiron.nix
+++ pkgs/nixos/configurations/personal-inspiron.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
   imports = [ ../profiles/personal.nix ../hardware/inspiron.nix ];
   machines.base.nixosState = "22.05";
-  machines.base.wifiInterfaceName = "TODO"; # ^^^^
+  machines.base.wifiInterfaceName = "wlp1s0";
   machines.base.bootMntPt = "/boot/efi";
   networking.hostName = "atorgesen-inspiron";
 }
