615562909c8cdb59618a91fa383613087a1077ce
goromal (bot) <goromal.bot@gmail.com>
2026-01-16 20:43:29 +0000

    Lint format

--- pkgs/nixos/components/x86-graphical-dev-pkgs.nix
+++ pkgs/nixos/components/x86-graphical-dev-pkgs.nix
@@ -24,38 +24,38 @@ in {
         "terminal.integrated.env.linux" = { "TMPDIR" = "/tmp"; };
       };
       extensions = with unstable.vscode-extensions;
-      [
-        eamodio.gitlens
-        ms-python.vscode-pylance
-        rust-lang.rust-analyzer
-        jnoortheen.nix-ide
-        yzhang.markdown-all-in-one
-        xaver.clang-format
-        ms-python.python
-        valentjn.vscode-ltex
-        b4dm4n.vscode-nixpkgs-fmt
-        zxh404.vscode-proto3
-        ms-vscode.cpptools
-      ] ++ unstable.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "cmake";
-          publisher = "twxs";
-          version = "0.0.17";
-          sha256 = "11hzjd0gxkq37689rrr2aszxng5l9fwpgs9nnglq3zhfa1msyn08";
-        }
-        {
-          name = "vscode-rustfmt";
-          publisher = "statiolake";
-          version = "0.1.2";
-          sha256 = "0kprx45j63w1wr776q0cl2q3l7ra5ln8nwy9nnxhzfhillhqpipi";
-        }
-        {
-          name = "claude-code";
-          publisher = "anthropic";
-          version = "2.0.34";
-          sha256 = "sha256-e+pjuGY0xrg43+pDDkQ4Svb1yBx2Fv+Z8WZoJv/k6D4=";
-        }
-      ];
+        [
+          eamodio.gitlens
+          ms-python.vscode-pylance
+          rust-lang.rust-analyzer
+          jnoortheen.nix-ide
+          yzhang.markdown-all-in-one
+          xaver.clang-format
+          ms-python.python
+          valentjn.vscode-ltex
+          b4dm4n.vscode-nixpkgs-fmt
+          zxh404.vscode-proto3
+          ms-vscode.cpptools
+        ] ++ unstable.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "cmake";
+            publisher = "twxs";
+            version = "0.0.17";
+            sha256 = "11hzjd0gxkq37689rrr2aszxng5l9fwpgs9nnglq3zhfa1msyn08";
+          }
+          {
+            name = "vscode-rustfmt";
+            publisher = "statiolake";
+            version = "0.1.2";
+            sha256 = "0kprx45j63w1wr776q0cl2q3l7ra5ln8nwy9nnxhzfhillhqpipi";
+          }
+          {
+            name = "claude-code";
+            publisher = "anthropic";
+            version = "2.0.34";
+            sha256 = "sha256-e+pjuGY0xrg43+pDDkQ4Svb1yBx2Fv+Z8WZoJv/k6D4=";
+          }
+        ];
     };
     mutableExtensionsDir = false;
   };
