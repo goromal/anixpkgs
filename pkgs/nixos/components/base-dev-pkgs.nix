46f6ad6f8d6239236e94eac5e66586d9265975ce
Andrew Torgesen <atorgesen@anduril.com>
2026-01-23 14:23:28 -0800

    [dev] Update claude-setup

--- pkgs/nixos/components/base-dev-pkgs.nix
+++ pkgs/nixos/components/base-dev-pkgs.nix
@@ -19,6 +19,7 @@ in {
     anixpkgs.py-helper
     anixpkgs.rust-helper
     anixpkgs.makepyshell
+    pkgs.gh
     pkgs.universal-ctags
     unstable.claude-code
     (pkgs.writeShellScriptBin "claude-setup" ''
@@ -29,8 +30,17 @@ in {
       echo_yellow "Installing claude plugins..."
       claude plugin marketplace add DevonMorris/claude-ctags 2>/dev/null || true
       claude plugin install claude-ctags@claude-ctags 2>/dev/null || true
+      claude plugin install code-review@claude-plugins-official
+      claude plugin install frontend-design@claude-plugins-official
+      claude plugin install github@claude-plugins-official
+      claude plugin install feature-dev@claude-plugins-official
+      claude plugin install pr-review-toolkit@claude-plugins-official
       echo_green "Done! Verify installed plugins with \"claude plugin list\""
+      echo_yellow "Other setup..."
+      gh auth login
     '')
+    # ^^^^ ✘ Failed to install plugin "code-review@claude-plugins-official": Failed to update settings: Failed to read raw settings from /data/andrew/.claude/settings.json: Error: EROFS: read-only file system, open '/nix/store/bx0r3qc2vas9h64fwvpfh09wjyzldb6a-home-manager-files/.claude/settings.json'
+
   ];
 
   programs.git = {
