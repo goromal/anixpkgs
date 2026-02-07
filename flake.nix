dd469d3ff5ef0fd62dd166674885cfc861b73dc1
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-02-06 20:45:01 -0800

    More wallpapers

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    anixdata.url = "github:goromal/anixdata";
+    anixdata.url = "github:goromal/anixdata"; 
     anixdata.flake = false;
 
     aapis.url = "github:goromal/aapis";
