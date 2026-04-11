d0e19bfe2e43dd851150472a31eed4c695c07b88
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-04-11 16:33:12 -0700

    ok

--- pkgs/rust-packages/rtk/default.nix
+++ pkgs/rust-packages/rtk/default.nix
@@ -7,7 +7,8 @@ rustPlatform.buildRustPackage rec {
   pname = "rtk";
   version = "0.1.0";
   src = pkg-src;
-  cargoHash = lib.fakeHash;
+  cargoHash = "sha256-r/PCA15MsmERCq3z8nObxdbX3KijsrInxsgJ6aqRVc4=";
+  doCheck = false;
   meta = {
     description = "CLI proxy that reduces LLM token consumption by filtering and compressing command outputs.";
     longDescription = ''
