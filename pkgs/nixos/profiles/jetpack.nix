d771682d0ebc57ff9ee55e66baf9661e336866e7
goromal (bot) <goromal.bot@gmail.com>
2026-05-07 04:07:56 +0000

    Lint format

--- pkgs/nixos/profiles/jetpack.nix
+++ pkgs/nixos/profiles/jetpack.nix
@@ -79,18 +79,19 @@ with import ../dependencies.nix;
       ];
     })
     // {
-      services.launchpad.pythonPackages = ps: with ps; [
-        numpy
-        scipy
-        torch
-        opencv4
-        geometry
-        pysignals
-        pyceres
-        pyceres_factors
-        mesh-plotter
-        find_rotational_conventions
-      ];
+      services.launchpad.pythonPackages =
+        ps: with ps; [
+          numpy
+          scipy
+          torch
+          opencv4
+          geometry
+          pysignals
+          pyceres
+          pyceres_factors
+          mesh-plotter
+          find_rotational_conventions
+        ];
 
       users.users.andrew.hashedPassword = lib.mkForce "$6$Kof8OUytwcMojJXx$vc82QBfFMxCJ96NuEYsrIJ0gJORjgpkeeyO9PzCBgSGqbQePK73sa13oK1FGY1CGd09qbAlsdiXWmO6m9c3K.0";
       environment.systemPackages = [
