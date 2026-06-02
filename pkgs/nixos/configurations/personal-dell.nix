9d0ed667d462334091d20d18ddd95694e0fac4bc
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-06-01 22:22:33 -0700

    Add personal-dell (#526)
    Co-authored-by: goromal (bot) <goromal.bot@gmail.com>

---
+++ pkgs/nixos/configurations/personal-dell.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ../profiles/personal.nix
+    ../hardware/dell.nix
+  ];
+  machines.base.nixosState = "25.11";
+  # machines.base.wifiInterfaceName = "wlo1"; TODO - test with adapter
+  machines.base.acceptRemoteBuilds = true;
+  networking.hostName = "atorgesen-dell";
+}
