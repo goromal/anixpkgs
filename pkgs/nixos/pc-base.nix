d8018ef77864d32d8ee50cd0cea7203cd215f857
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-06-03 13:33:23 -0700

    [anix-init] genericize for all profiles

--- pkgs/nixos/pc-base.nix
+++ pkgs/nixos/pc-base.nix
@@ -28,6 +28,61 @@ let
     rcloneCfg = "${cfg.homeDir}/.config/rclone/rclone.conf";
   };
   machine-authm = anixpkgs.authm.override { rcrsync = machine-rcrsync; };
+  anix-init = pkgs.writeShellScriptBin "anix-init" ''
+    make-title -c yellow "Setting up rcrsync"
+
+    DO_RCLONE=y
+    if [[ -f $HOME/.config/rclone/rclone.conf ]]; then
+      read -rp "rclone config already found, proceed anyway? (y|n): " DO_RCLONE
+    fi
+    if [[ "$DO_RCLONE" == "y" ]]; then
+      read -rp "Enter the char key to unlock the rclone config: " CFGKEY
+      rm -rf $HOME/.config/rclone
+      mkdir -p $HOME/.config/rclone && cd $HOME/.config/rclone
+      cp ${anixpkgs.pkgData.records.rcloneConf.data} ${anixpkgs.pkgData.records.rcloneConf.name}
+      sunnyside -s 0 -k $CFGKEY -t ${anixpkgs.pkgData.records.rcloneConf.name}
+      rm ${anixpkgs.pkgData.records.rcloneConf.name}
+    else
+      echo_yellow "Skipping rclone config step"
+    fi
+
+    cd $HOME
+    ${lib.concatMapStrings (dir: "rcrsync -v init ${dir.name}\n") cfg.cloudDirs}
+    make-title -c yellow "Setting up SSH and Nix"
+
+    DO_SSH=y
+    if [[ -d $HOME/.ssh ]]; then
+      read -rp ".ssh directory already present, proceed anyway? (y|n): " DO_SSH
+    fi
+    if [[ "$DO_SSH" == "y" ]]; then
+      rm -rf $HOME/.ssh
+      cp -r $HOME/data/.ssh $HOME/.ssh
+      cd $HOME/.ssh
+      fix-perms .
+      cd ..
+    else
+      echo_yellow "Skipping SSH config setup"
+    fi
+
+    sudo nix-channel --add https://nixos.org/channels/nixos-${nixos-version} nixpkgs
+    sudo nix-channel --add https://nixos.org/channels/nixos-${nixos-version} nixos
+    sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-${nixos-version}.tar.gz home-manager
+    sudo nix-channel --update
+    echo
+    echo
+    echo_green "DONE. Note the hardware-config.nix file below:"
+    echo
+    nixos-generate-config --show-hardware-config
+    echo
+    echo_green  "Use the config above as you set up anix-upgrade:"
+    echo_yellow "  - Use devshell to create a workspace with anixpkgs"
+    echo_yellow "  - Use the config above to define a new configuration in anixpkgs"
+    echo_yellow "  - Symlink /etc/nixos/configuration.nix to $HOME/sources"
+    echo_yellow "  - Run anix-upgrade"
+    echo_yellow "  - Create new secrets and configs entries"
+    echo
+    echo_green "Have fun!"
+  '';
 in
 {
   options.machines.base = {
@@ -884,7 +939,8 @@ in
         else
           [ ]
       )
-      ++ (if cfg.runWebServer then [ anixpkgs.generate-local-ssl-certs ] else [ ]);
+      ++ (if cfg.runWebServer then [ anixpkgs.generate-local-ssl-certs ] else [ ])
+      ++ [ anix-init ];
 
     programs.bash.interactiveShellInit = ''
       ${if cfg.developer then ''eval "$(direnv hook bash)"'' else ""}
