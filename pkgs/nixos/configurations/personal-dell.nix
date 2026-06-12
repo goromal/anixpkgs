da6ca71e1492c74ecadae46b462ea94c04b15d68
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-06-11 20:23:16 -0700

    [dell] wifi interface name

--- pkgs/nixos/configurations/personal-dell.nix
+++ pkgs/nixos/configurations/personal-dell.nix
@@ -10,7 +10,7 @@
     ../hardware/dell.nix
   ];
   machines.base.nixosState = "25.11";
-  # machines.base.wifiInterfaceName = "wlo1"; TODO - test with adapter
+  machines.base.wifiInterfaceName = "wlp0s13f0u1u4";
   machines.base.acceptRemoteBuilds = true;
   machines.cudaNode.enable = true;
   networking.hostName = "atorgesen-dell";
