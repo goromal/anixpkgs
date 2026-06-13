7602702496e828fdf3474695f1057cb88e271238
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-06-13 08:32:26 -0700

    [dev] Bump claude code version

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -622,6 +622,6 @@ rec {
 
   multirotor-sim = prev.callPackage ./nixos/multirotor/run.nix baseModuleArgs;
 
-  # Override claude-code-bin to use version 2.1.113
+  # Override claude-code-bin to use version 2.1.177
   claude-code-bin = prev.callPackage ./by-name/cl/claude-code-bin/package.nix { };
 }
