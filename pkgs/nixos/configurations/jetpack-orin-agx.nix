acb3946dfe53b6b82e0afde073ef2a93b877ba93
Andrew Torgesen <andrew.torgesen@gmail.com>
2026-07-23 19:45:32 -0700

    Add new orin machine

---
+++ pkgs/nixos/configurations/jetpack-orin-agx.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ../profiles/jetpack.nix
+    ../hardware/orin-agx.nix
+  ];
+  machines.base.nixosState = "25.11";
+  machines.base.wifiInterfaceName = "wlP1p1s0";
+  hardware.nvidia-jetpack.som = "orin-agx";
+  hardware.nvidia-jetpack.carrierBoard = "devkit";
+  networking.hostName = "jetson-orin-agx";
+
+  services.comfyui.cozy.workflows = [
+    "imggen"
+    "imggen2"
+    "imgedit3"
+  ];
+
+  nix.settings.max-jobs = lib.mkForce 1;
+  nix.settings.cores = lib.mkForce 4;
+
+  machines.base.remoteBuilders = [
+    "personal-inspiron"
+    "personal-panasonic"
+    "personal-dell"
+  ];
+}
